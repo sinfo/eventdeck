06be50c036a387efc9b32efc6ac58b8db20925ca
Francisco Baio Dias <xicombd@gmail.com>
2014-04-20 01:55:36 +0100

    edit on client

---
+++ serverApp/db/index.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var mongo_url = 'mongodb://localhost/sinfo';
+
+require('./models/company');
+require('./models/member');
+
+mongoose.connect(mongo_url);
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (){
+  console.log('Successfuly connected to mongoDB');
+});
+
+module.exports = db;
---
+++ serverApp/scripts/createCompaniesFromJson.js
@@ -0,0 +1,34 @@
+var async = require('async');
+var companies = require('./companies');
+var Company   = require('./../db/models/company.js');
+require('./../db');
+
+setTimeout(function() {
+  for (var i = 0; i < companies.length; i++) {
+    var company = {};
+
+    company.id   = companies[i].id;
+    company.name = companies[i].name;
+    if (companies[i].img)          { company.img         = companies[i].img; }
+    if (companies[i].description)  { company.description = companies[i].description; }
+    if (companies[i].status)       { company.status      = companies[i].status; }
+    if (companies[i].history)      { company.history     = companies[i].history; }
+    if (companies[i].contacts)     { company.contacts    = companies[i].contacts; }
+    if (companies[i].forum)        { company.forum       = companies[i].forum; }
+    if (companies[i].status)       { company.status      = companies[i].status; }
+    if (companies[i].member)       { company.member      = companies[i].member; }
+    if (companies[i].area)         { company.area        = companies[i].area; }
+    
+    var newCompany = new Company(company);
+
+    newCompany.save(function (err, reply){
+      if (err) {
+        console.log("ERROR",Hapi.error.internal('Hipcup on the DB' + err.detail));
+      } else if(reply) {
+        console.log("SUCCESS", company);
+      } else { // same id        
+        console.log("ERROR",Hapi.error.conflict('Company ID exists: '+request.payload.id));
+      }
+    });
+  }
+}, 3000);
