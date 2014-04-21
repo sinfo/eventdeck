07923e258eaef3df6452cf56548aa2ac4ab37328
Francisco Baio Dias <xicombd@gmail.com>
2014-04-21 01:39:07 +0100

    pretty lists

--- clientApp/js/angularApp/controllers/company/list.js
+++ clientApp/js/angularApp/controllers/company/list.js
@@ -6,8 +6,18 @@ theToolController
       return $sce.trustAsResourceUrl(src);
     }
 
+    $scope.getClassFromKind = function(participation) {
+      if(!participation) { return "nope"; }
+      if(!participation.kind) { return "sponsor"; }
+      var kind = participation.kind.toLowerCase();
+
+      if(kind.indexOf("bronze") != -1) { return "bronze"; } 
+      else if(kind.indexOf("prata") != -1) { return "silver"; }
+      else { return "sponsor"; }
+    }
+
     CompanyFactory.getAll(function(response) {
-      $scope.predicate = 'status';
+      $scope.predicate = 'participation';
       $scope.reverse = false;
       $scope.companies = response;
     });
--- clientApp/js/angularApp/controllers/member/member.js
+++ clientApp/js/angularApp/controllers/member/member.js
@@ -2,6 +2,15 @@
  
 theToolController
   .controller('MemberController', function ($scope, $http, $routeParams, $sce, MemberFactory) {
+    $scope.getClassFromKind = function(participation) {
+      if(!participation) { return "nope"; }
+      if(!participation.kind) { return "sponsor"; }
+      var kind = participation.kind.toLowerCase();
+
+      if(kind.indexOf("bronze") != -1) { return "bronze"; } 
+      else if(kind.indexOf("prata") != -1) { return "silver"; }
+      else { return "sponsor"; }
+    }
     
     MemberFactory.Member.get({id: $routeParams.id}, function(response) {
       $scope.member = response;
