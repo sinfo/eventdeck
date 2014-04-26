539e6f985f4a34c4ffd9ee7a8ae173ec0d70872d
Francisco Baio Dias <xicombd@gmail.com>
2014-04-26 01:50:02 +0100

    comments

---
+++ serverApp/routes/comment.js
@@ -0,0 +1,49 @@
+var server  = require('./../index.js');
+var comment = require('./../resources/comment');
+
+server.route({ 
+  method: 'GET', 
+  path: '/api/comment', 
+  config: { 
+    handler: comment.list, 
+    auth: true 
+  } 
+});
+
+server.route({ 
+  method: 'POST', 
+  path: '/api/comment', 
+  config: { 
+    handler: comment.create, 
+    auth: true 
+  } 
+});
+
+server.route({ 
+  method: 'DELETE', 
+  path: '/api/comment/{id}', 
+  config: { 
+    handler: comment.delete, 
+    auth: true 
+  } 
+});
+/*
+server.route({ 
+  method: 'GET', 
+  path: '/api/comment/{id}', 
+  config: { 
+    handler: comment.get, 
+    auth: true 
+  } 
+});
+
+server.route({ 
+  method: 'PUT', 
+  path: '/api/comment/{id}', 
+  config: { 
+    handler: comment.update, 
+    auth: true 
+  } 
+});
+
+*/
