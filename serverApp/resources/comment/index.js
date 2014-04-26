ff32ecd40bd64c0a0e1a744de172734925ab4a40
Francisco Baio Dias <xicombd@gmail.com>
2014-04-26 14:14:47 +0100

    much edit

--- serverApp/resources/comment/index.js
+++ serverApp/resources/comment/index.js
@@ -1,7 +1,7 @@
 exports.list            = require('./list');
 exports.create          = require('./create');
 exports.delete          = require('./delete');
-//exports.update          = require('./update');
+exports.update          = require('./update');
 exports.get             = require('./get');
 //exports.getByMember     = require('./getByMember');
 exports.getByThread     = require('./getByThread');
