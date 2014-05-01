90d079b357712208c2cc68ced42502326e7ed4b3
Makay11 <dpais11@gmail.com>
2014-05-01 23:07:59 +0100

    added speaker comments

--- clientApp/js/angularApp/controllers/speaker/speaker.js
+++ clientApp/js/angularApp/controllers/speaker/speaker.js
@@ -1,7 +1,7 @@
 'use strict';
- 
+
 theToolController
-  .controller('SpeakerController', function ($scope, $http, $routeParams, $sce, SpeakerFactory, MemberFactory) {
+  .controller('SpeakerController', function ($scope, $http, $routeParams, $sce, SpeakerFactory, MemberFactory, CommentFactory) {
     $scope.trustSrc = function(src) {
       return $sce.trustAsResourceUrl(src+'#page-body');
     }
@@ -9,7 +9,13 @@ theToolController
       var urlExp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
       var mailExp = /[\w\.\-]+\@([\w\-]+\.)+[\w]{2,4}(?![^<]*>)/ig;
 
-      return text.replace(/\n/g, '<br>').replace(urlExp,"<a href='$1'>$1</a>").replace(mailExp,"<a href='mailto:$&'>$&</a>");  
+      return text.replace(/\n/g, '<br>').replace(urlExp,"<a href='$1'>$1</a>").replace(mailExp,"<a href='/#/speaker/"+$routeParams.id+"/confirm?email=$&'>$&</a>");
+    }
+    $scope.convertNewLinesToHtml = function(text) {
+      return '<div data-markdown>'+text.replace(/\n/g, '<br>')+'</div>';
+    }
+    $scope.convertMarkdownToHtml = function(text) {
+      return '<div data-markdown>'+text+'</div>';
     }
     $scope.submit = function() {
       var speakerData = this.formData;
@@ -23,15 +29,57 @@ theToolController
       });
     };
 
+    $scope.getMemberFacebook = function(id) {
+      return $scope.members.filter(function(e){
+          return e.id == id;
+        })[0].facebook;
+    }
+
+    $scope.submitComment = function() {
+      $scope.loading = true;
+
+      var commentData = this.commentData;
+      commentData.thread = 'speaker-'+this.speaker.id;
+
+      CommentFactory.Comment.create(commentData, function(data) {
+        // if successful, we'll need to refresh the comment list
+        CommentFactory.Speaker.getAll({id: $routeParams.id}, function(getData) {
+          $scope.comments = getData;
+          $scope.loading = false;
+        });
+      });
+    };
+
+    $scope.deleteComment = function(id) {
+      $scope.loading = true;
+
+      CommentFactory.Comment.delete({id: id}, function(data) {
+        // if successful, we'll need to refresh the comment list
+        CommentFactory.Speaker.getAll({id: $routeParams.id}, function(getData) {
+          $scope.comments = getData;
+          $scope.loading = false;
+        });
+      });
+    };
+
+    $scope.quoteComment = function(comment) {
+      $scope.commentData.markdown = '> **'+comment.member+' said**:\n> ' + comment.markdown.split('\n').join('\n> ')+'\n';
+    };
+
     SpeakerFactory.get({id: $routeParams.id}, function(response) {
       $scope.speaker = $scope.formData = response;
-      
+
       MemberFactory.Member.getAll( function(response) {
         $scope.members = response;
         $scope.member = $scope.members.filter(function(e){
           return e.id == $scope.formData.member;
         })[0];
         console.log($scope.member);
+      });
+
+      CommentFactory.Speaker.getAll({id: $routeParams.id}, function(getData) {
+        $scope.comments = getData;
+        $scope.loading = false;
       });
     });
   });
