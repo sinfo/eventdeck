7b533596e8947cbea114a24beffdc3320ba166e2
Makay11 <dpais11@gmail.com>
2014-05-03 19:36:39 +0100

    started working on the sponsor page

---
+++ serverApp/resources/company/sponsorPage.js
@@ -0,0 +1,9 @@
+var Hapi           = require('hapi');
+var async          = require('async');
+var Company        = require('./../../db/models/company.js');
+
+exports = module.exports = page;
+
+function page(request, reply) {
+	reply("Test");
+}
