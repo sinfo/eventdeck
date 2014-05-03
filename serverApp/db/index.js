b4dfc2845218fdc068b0d03ee9c68776f83e87e1
Makay11 <dpais11@gmail.com>
2014-05-03 14:50:51 +0100

    removed a few lines

--- serverApp/db/index.js
+++ serverApp/db/index.js
@@ -2,11 +2,6 @@ var mongoose = require('mongoose');
 
 var mongo_url = 'mongodb://localhost/sinfo';
 
-require('./models/company');
-require('./models/member');
-require('./models/speaker');
-require('./models/comment');
-
 mongoose.connect(mongo_url);
 var db = mongoose.connection;
 
