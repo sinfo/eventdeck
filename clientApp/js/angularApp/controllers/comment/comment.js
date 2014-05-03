1be873e5dbb9734ab6a77822d774c30de44a16e8
Makay11 <dpais11@gmail.com>
2014-05-03 16:55:32 +0100

    fixed undefined preview bug

--- clientApp/js/angularApp/controllers/comment/comment.js
+++ clientApp/js/angularApp/controllers/comment/comment.js
@@ -1,19 +1,19 @@
 'use strict';
- 
+
 theToolController
   .controller('CommentController', function ($scope, $http, $routeParams, $sce, CommentFactory) {
     $scope.convertNewLinesToHtml = function(text) {
-      return text.replace(/\n/g, '<br>');  
+      return text.replace(/\n/g, '<br>');
     }
     $scope.convertMarkdownToHtml = function(text) {
-      return '<div data-markdown>'+text+'</div>';  
+      return '<div data-markdown>' + text + '</div>';
     }
 
     $scope.submitComment = function() {
       $scope.loading = true;
 
       var commentData = this.commentData;
-      
+
       CommentFactory.Comment.update({ id:commentData._id }, commentData, function(response) {
         // if successful, we'll need to refresh the comment list
         console.log(response);
