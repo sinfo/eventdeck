a6b6d6b803647438e1261ca27079b4402dcd7d49
Makay11 <dpais11@gmail.com>
2014-05-04 21:30:33 +0100

    added timeline

--- clientApp/js/angularApp/controllers/main/home.js
+++ clientApp/js/angularApp/controllers/main/home.js
@@ -1,15 +1,18 @@
 'use strict';
 
-theToolController
-  .controller('home', function ($scope, $http, $sce, CompanyFactory) {
-    $scope.trustSrc = function(src) {
-      return $sce.trustAsResourceUrl(src);
-    }
+theToolController.controller('home', function ($scope, $http, $sce, NotificationFactory) {
+  $scope.trustSrc = function(src) {
+    return $sce.trustAsResourceUrl(src);
+  }
 
-    CompanyFactory.getAll(function(response) {
-      $scope.predicate = 'status';
-      $scope.reverse = false;
-      $scope.companies = response;
-    });
+  $scope.notifications = [];
+
+  NotificationFactory.getAll(function(response) {
+    for (var notification in response){
+      $scope.notifications.unshift({
+        path: response[notification].thread.replace("-", "/"),
+        text: response[notification].description
+      });
+    }
   });
-  
+});
