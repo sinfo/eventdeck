a126ef8f128f87adafaec6b936c8317d6ecc6a01
Makay11 <dpais11@gmail.com>
2014-05-05 03:29:27 +0100

    notifications thingy

--- clientApp/js/angularApp/controllers/index.js
+++ clientApp/js/angularApp/controllers/index.js
@@ -1,7 +1,8 @@
 theToolController = angular.module('theTool.controllers', []);
- 
+
 require('./main');
 require('./company');
 require('./speaker');
 require('./member');
 require('./comment');
+require('./notification');
