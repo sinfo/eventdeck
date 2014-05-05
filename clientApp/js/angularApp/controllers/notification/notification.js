fdc96922ec161c58efab10f25e52c6185764ac9e
Francisco Baio Dias <xicombd@gmail.com>
2014-05-05 14:16:59 +0100

    menus pipis

--- clientApp/js/angularApp/controllers/notification/notification.js
+++ clientApp/js/angularApp/controllers/notification/notification.js
@@ -1,6 +1,6 @@
 'use strict';
 
-theToolController.controller('NotificationController', function ($scope, $http, $routeParams, $sce, NotificationFactory, MemberFactory) {
+theToolController.controller('NotificationController', function ($scope, $http, $routeParams, $sce, $location, $rootScope, NotificationFactory, MemberFactory) {
 
   $scope.loading = true;
 
@@ -11,9 +11,53 @@ theToolController.controller('NotificationController', function ($scope, $http, 
     text: " Loading..."
   };
 
+  $scope.me = {};
+  $scope.members = [];
+
+  $rootScope.$on("$locationChangeStart", function(event, next, current) { 
+    setTimeout($scope.update, 500);
+  });
+
+  $scope.update = function() {
+    NotificationFactory.getAll(function(response) {
+      $scope.notifications = [];
+      $scope.notificationsInfo.number = 0;
+      
+      for (var i = 0, j = response.length; i < j; i++) {
+        //if (response[i].member != me.id) { //uncomment to hide self-events
+        if (response[i].unread.indexOf($scope.me.id) != -1) {
+          $scope.notificationsInfo.number++;
+
+          $scope.notifications.unshift({
+            path: response[i].thread.replace("-", "/"),
+            text: response[i].description,
+            member: $scope.members.filter(function(o) {
+                      return response[i].member == o.id;
+                    })[0].facebook,
+            color: (response[i].unread.indexOf($scope.me.id) != -1 ? "green" : "grey")
+          });
+        }
+        //}
+      }
+
+      $scope.loading = false;
+
+      if ($scope.notificationsInfo.number == 0) {
+        $scope.notificationsInfo.text = " No Notifications";
+      }
+      else {
+        $scope.notificationsInfo.text = " " + $scope.notificationsInfo.number + " Notification" + ($scope.notificationsInfo.number > 1 ? "s" : "");
+      }
+    });
+  }
+
   MemberFactory.Member.get({id: "me"}, function(me) {
 
+    $scope.me = me;
+
     MemberFactory.Member.getAll(function(members) {
+
+    $scope.members = members;
 
       NotificationFactory.getAll(function(response) {
         for (var i = 0, j = response.length; i < j; i++) {
@@ -43,47 +87,7 @@ theToolController.controller('NotificationController', function ($scope, $http, 
         }
       });
 
-      var update = function() {
-
-        setTimeout(function() {
-
-          NotificationFactory.getAll(function(response) {
-            $scope.notifications = [];
-            $scope.notificationsInfo.number = 0;
-            
-            for (var i = 0, j = response.length; i < j; i++) {
-              //if (response[i].member != me.id) { //uncomment to hide self-events
-              if (response[i].unread.indexOf(me.id) != -1) {
-                $scope.notificationsInfo.number++;
-
-                $scope.notifications.unshift({
-                  path: response[i].thread.replace("-", "/"),
-                  text: response[i].description,
-                  member: members.filter(function(o) {
-                            return response[i].member == o.id;
-                          })[0].facebook,
-                  color: (response[i].unread.indexOf(me.id) != -1 ? "green" : "grey")
-                });
-              }
-              //}
-            }
-
-            $scope.loading = false;
-
-            if ($scope.notificationsInfo.number == 0) {
-              $scope.notificationsInfo.text = " No Notifications";
-            }
-            else {
-              $scope.notificationsInfo.text = " " + $scope.notificationsInfo.number + " Notification" + ($scope.notificationsInfo.number > 1 ? "s" : "");
-            }
-          });
-
-          update();
-        }, 5000);
-
-      };
-
-      update();
+      setInterval($scope.update, 10000);
     });
   });
 });
