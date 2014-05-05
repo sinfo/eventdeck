749abd4ae32cde52fc2ff0403c28772c2e7f9a67
Makay11 <dpais11@gmail.com>
2014-05-05 04:53:04 +0100

    notifications thingy auto-refreshing

--- clientApp/js/angularApp/controllers/notification/notification.js
+++ clientApp/js/angularApp/controllers/notification/notification.js
@@ -11,20 +11,20 @@ theToolController.controller('NotificationController', function ($scope, $http, 
     text: " Loading..."
   };
 
-  MemberFactory.Member.get({id: "me"}, function(me){
+  MemberFactory.Member.get({id: "me"}, function(me) {
 
-    MemberFactory.Member.getAll(function(members){
+    MemberFactory.Member.getAll(function(members) {
 
-      NotificationFactory.getAll(function(response){
-        for (var i = 0, j = response.length; i < j; i++){
-          //if (response[i].member != me.id){ //uncomment to hide self-events
-          if (response[i].unread.indexOf(me.id) != -1){
+      NotificationFactory.getAll(function(response) {
+        for (var i = 0, j = response.length; i < j; i++) {
+          //if (response[i].member != me.id) { //uncomment to hide self-events
+          if (response[i].unread.indexOf(me.id) != -1) {
             $scope.notificationsInfo.number++;
 
             $scope.notifications.unshift({
               path: response[i].thread.replace("-", "/"),
               text: response[i].description,
-              member: members.filter(function(o){
+              member: members.filter(function(o) {
                         return response[i].member == o.id;
                       })[0].facebook,
               color: (response[i].unread.indexOf(me.id) != -1 ? "green" : "grey")
@@ -35,13 +35,61 @@ theToolController.controller('NotificationController', function ($scope, $http, 
 
         $scope.loading = false;
 
-        if ($scope.notificationsInfo.number == 0){
+        if ($scope.notificationsInfo.number == 0) {
           $scope.notificationsInfo.text = " No notifications";
         }
-        else{
+        else {
           $scope.notificationsInfo.text = " " + $scope.notificationsInfo.number + " notification" + ($scope.notificationsInfo.number > 1 ? "s" : "");
         }
       });
+
+      var update = function() {
+
+        setTimeout(function() {
+
+          $scope.loading = true;
+
+          $scope.notifications = [];
+
+          $scope.notificationsInfo = {
+            number: 0,
+            text: " Loading..."
+          };
+
+          NotificationFactory.getAll(function(response) {
+            for (var i = 0, j = response.length; i < j; i++) {
+              //if (response[i].member != me.id) { //uncomment to hide self-events
+              if (response[i].unread.indexOf(me.id) != -1) {
+                $scope.notificationsInfo.number++;
+
+                $scope.notifications.unshift({
+                  path: response[i].thread.replace("-", "/"),
+                  text: response[i].description,
+                  member: members.filter(function(o) {
+                            return response[i].member == o.id;
+                          })[0].facebook,
+                  color: (response[i].unread.indexOf(me.id) != -1 ? "green" : "grey")
+                });
+              }
+              //}
+            }
+
+            $scope.loading = false;
+
+            if ($scope.notificationsInfo.number == 0) {
+              $scope.notificationsInfo.text = " No notifications";
+            }
+            else {
+              $scope.notificationsInfo.text = " " + $scope.notificationsInfo.number + " notification" + ($scope.notificationsInfo.number > 1 ? "s" : "");
+            }
+          });
+
+          update();
+        }, 15000);
+
+      };
+
+      update();
     });
   });
 });
