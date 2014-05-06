aa6279c94c348e2d849fddd04b5f42fcf068e04b
Francisco Baio Dias <xicombd@gmail.com>
2014-05-06 17:43:01 +0100

    time since on scope

--- clientApp/js/angularApp/controllers/main/home.js
+++ clientApp/js/angularApp/controllers/main/home.js
@@ -5,42 +5,12 @@ theToolController.controller('home', function ($scope, $http, $sce, Notification
     return $sce.trustAsResourceUrl(src);
   }
 
-  $scope.loading = true;
-  $scope.notifications = [];
-
-  MemberFactory.Member.get({id: "me"}, function(me) {
-
-    MemberFactory.Member.getAll(function(members) {
-
-      NotificationFactory.getAll(function(response) {
-        for (var i = 0, j = response.length; i < j; i++) {
-          //if (response[i].member != me.id){ //uncomment to hide self-events
-          var date = new Date(response[i].posted);
-          $scope.notifications.unshift({
-            path: response[i].thread.replace("-", "/"),
-            date: {
-              full: date.toString(),
-              ago: timeSince(date)
-            },
-            description: response[i].description,
-            member: members.filter(function(o) {
-                      return response[i].member == o.id;
-                    })[0].facebook,
-            color: (response[i].unread.indexOf(me.id) != -1 ? "green" : "grey")
-          });
-          //}
-        }
-
-        $scope.loading = false;
-      });
-    });
-  });
-});
-
-function timeSince(date) {
-    var seconds = Math.floor((new Date() - date) / 1000);
+  $scope.timeSince =function (date) {
+    var seconds = Math.floor((Date.now() - date) / 1000);
 
     var interval = Math.floor(seconds / 31536000);
+
+    console.log(date, seconds, interval);
 
     if (interval > 1) {
         return interval + " years ago";
@@ -62,4 +32,36 @@ function timeSince(date) {
         return interval + " minutes ago";
     }
     return Math.floor(seconds) + " seconds ago";
-}
+  }
+
+  $scope.loading = true;
+  $scope.notifications = [];
+
+  MemberFactory.Member.get({id: "me"}, function(me) {
+
+    MemberFactory.Member.getAll(function(members) {
+
+      NotificationFactory.getAll(function(response) {
+        for (var i = 0, j = response.length; i < j; i++) {
+          //if (response[i].member != me.id){ //uncomment to hide self-events
+          var date = new Date(response[i].posted);
+          $scope.notifications.unshift({
+            path: response[i].thread.replace("-", "/"),
+            date: {
+              data: date,
+              full: date.toString()
+            },
+            description: response[i].description,
+            member: members.filter(function(o) {
+                      return response[i].member == o.id;
+                    })[0].facebook,
+            color: (response[i].unread.indexOf(me.id) != -1 ? "green" : "grey")
+          });
+          //}
+        }
+
+        $scope.loading = false;
+      });
+    });
+  });
+});
