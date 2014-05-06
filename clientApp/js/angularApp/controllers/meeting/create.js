0c36edd8d341bdcbb17ba0caec0c347cc83dddd8
Makay11 <dpais11@gmail.com>
2014-05-06 21:34:12 +0100

    meetings

--- clientApp/js/angularApp/controllers/meeting/create.js
+++ clientApp/js/angularApp/controllers/meeting/create.js
@@ -2,16 +2,31 @@
 
 theToolController.controller('CreateMeetingController', function ($scope, $http, $routeParams, $sce, $location, $rootScope, MeetingFactory, MemberFactory) {
 
-  $scope.success = "";
-  $scope.error   = "";
-
+  $scope.success  = "";
+  $scope.error    = "";
   $scope.formData = {};
+
+  MemberFactory.Member.get({id: "me"}, function(me) {
+    $scope.author = me;
+  });
 
   MemberFactory.Member.getAll(function(members) {
     $scope.members = members;
   });
 
   $scope.submit = function() {
+    $scope.formData.attendants = [];
+    for (var i = 0, j = $scope.members.length; i < j; i++) {
+      if ($scope.members[i].attending) {
+        $scope.formData.attendants.push($scope.members[i].id);
+      }
+    }
+
+    if (!$scope.formData.title){
+      $scope.error = "Please enter a title.";
+      return;
+    }
+
     /*MeetingFactory.create($scope.formData, function(response) {
       if(response.error) {
         $scope.error = response.error;
