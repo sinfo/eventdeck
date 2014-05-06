aea726baeb6229b23988f6f02708e2b7928ffe3c
Makay11 <dpais11@gmail.com>
2014-05-06 13:17:09 +0100

    date

--- clientApp/js/angularApp/controllers/main/home.js
+++ clientApp/js/angularApp/controllers/main/home.js
@@ -8,18 +8,22 @@ theToolController.controller('home', function ($scope, $http, $sce, Notification
   $scope.loading = true;
   $scope.notifications = [];
 
-  MemberFactory.Member.get({id: "me"}, function(me){
+  MemberFactory.Member.get({id: "me"}, function(me) {
 
-    MemberFactory.Member.getAll(function(members){
+    MemberFactory.Member.getAll(function(members) {
 
-      NotificationFactory.getAll(function(response){
-        for (var i = 0, j = response.length; i < j; i++){
+      NotificationFactory.getAll(function(response) {
+        for (var i = 0, j = response.length; i < j; i++) {
           //if (response[i].member != me.id){ //uncomment to hide self-events
           var date = new Date(response[i].posted);
           $scope.notifications.unshift({
             path: response[i].thread.replace("-", "/"),
-            text: date.toLocaleDateString("pt-PT") + " - " + date.getHours() + ":" + date.getMinutes() + " -> " + response[i].description,
-            member: members.filter(function(o){
+            date: {
+              full: date.toString(),
+              ago: timeSince(date)
+            },
+            description: response[i].description,
+            member: members.filter(function(o) {
                       return response[i].member == o.id;
                     })[0].facebook,
             color: (response[i].unread.indexOf(me.id) != -1 ? "green" : "grey")
@@ -32,3 +36,30 @@ theToolController.controller('home', function ($scope, $http, $sce, Notification
     });
   });
 });
+
+function timeSince(date) {
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years ago";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months ago";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days ago";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours ago";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+}
