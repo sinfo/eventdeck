9d05389f0ef7a8f982aaf4d322cd5208227d1bca
Makay11 <dpais11@gmail.com>
2014-05-07 17:52:55 +0100

    meetings

--- clientApp/js/angularApp/controllers/meeting/list.js
+++ clientApp/js/angularApp/controllers/meeting/list.js
@@ -1,10 +1,23 @@
 'use strict';
 
-theToolController.controller('MeetingsController', function ($scope, MeetingFactory) {
+theToolController.controller('MeetingsController', function ($scope, MeetingFactory, MemberFactory) {
 
-  MeetingFactory.getAll(function(result) {
-  	console.log(result);
-  	$scope.meetings = result;
+  MeetingFactory.getAll(function(meetings) {
+  	$scope.meetings = meetings;
+
+  	MemberFactory.Member.getAll(function(members) {
+    	$scope.members = members;
+
+    	for (var i = 0, j = $scope.meetings.length; i < j; i++) {
+    		$scope.meetings[i].facebook = $scope.members.filter(function(o) {
+          return $scope.meetings[i].author == o.id;
+        })[0].facebook;
+    	}
+	 });
   });
+
+  $scope.time = function(date) {
+    return $scope.timeSince(new Date(date));
+  };
 
 });
