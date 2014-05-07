6f03f7de160ed174d3bdeb584c56a22e8d374f50
Makay11 <dpais11@gmail.com>
2014-05-07 02:01:57 +0100

    them meetings so fancy!

--- clientApp/js/angularApp/controllers/meeting/create.js
+++ clientApp/js/angularApp/controllers/meeting/create.js
@@ -1,10 +1,18 @@
 'use strict';
 
-theToolController.controller('CreateMeetingController', function ($scope, $http, $routeParams, $sce, $location, $rootScope, MeetingFactory, MemberFactory) {
+theToolController.controller('CreateMeetingController', function ($scope, MeetingFactory, MemberFactory) {
+
+  //================================INITIALIZATION================================
 
   $scope.success  = "";
   $scope.error    = "";
-  $scope.formData = {};
+  $scope.formData = {
+    title: new Date().toLocaleDateString("pt-PT") + " - Meeting"
+  };
+
+  $scope.noteTypes = ["Info", "To do", "Decision", "Idea"];
+
+  $scope.notes = [];
 
   MemberFactory.Member.get({id: "me"}, function(me) {
     $scope.author = me;
@@ -13,6 +21,42 @@ theToolController.controller('CreateMeetingController', function ($scope, $http,
   MemberFactory.Member.getAll(function(members) {
     $scope.members = members;
   });
+
+
+  //===================================FUNCTIONS===================================
+
+  $scope.toggleAttendant = function(member) {
+    member.attending = !member.attending;
+  };
+
+  $scope.toggleAttendants = function() {
+    for (var i = 0, j = $scope.members.length; i < j; i++) {
+      $scope.toggleAttendant($scope.members[i]);
+    }
+  };
+
+  $scope.addTarget = function(note) {
+    if (!note.targets)
+      note.targets = [];
+
+    note.targets.push({
+      name: "Teste"
+    });
+  };
+
+  $scope.removeTarget = function(target, targets) {
+    targets.splice(targets.indexOf(target), 1);
+  };
+
+  $scope.addNote = function() {
+    $scope.notes.push({
+      type: "Info"
+    });
+  };
+
+  $scope.removeNote = function(note) {
+    $scope.notes.splice($scope.notes.indexOf(note), 1);
+  };
 
   $scope.submit = function() {
     $scope.formData.attendants = [];
