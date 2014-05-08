591a7a0ed9dc35293f42d493c47997f3af3c4c20
Makay11 <dpais11@gmail.com>
2014-05-08 01:05:57 +0100

    meetings

--- clientApp/js/angularApp/controllers/meeting/list.js
+++ clientApp/js/angularApp/controllers/meeting/list.js
@@ -1,23 +1,42 @@
 'use strict';
 
-theToolController.controller('MeetingsController', function ($scope, MeetingFactory, MemberFactory) {
+theToolController.controller('MeetingsController', function ($scope, $location, MeetingFactory) {
+
+  //================================INITIALIZATION================================
+
+  $scope.loading = true;
 
   MeetingFactory.getAll(function(meetings) {
-  	$scope.meetings = meetings;
+    $scope.meetings = meetings;
 
-  	MemberFactory.Member.getAll(function(members) {
-    	$scope.members = members;
+    for (var i = 0, j = $scope.meetings.length; i < j; i++) {
+      $scope.meetings[i].facebook = $scope.members.filter(function(o) {
+        return $scope.meetings[i].author == o.id;
+      })[0].facebook;
+    }
 
-    	for (var i = 0, j = $scope.meetings.length; i < j; i++) {
-    		$scope.meetings[i].facebook = $scope.members.filter(function(o) {
-          return $scope.meetings[i].author == o.id;
-        })[0].facebook;
-    	}
-	 });
+    $scope.loading = false;
   });
+
+
+  //===================================FUNCTIONS===================================
 
   $scope.time = function(date) {
     return $scope.timeSince(new Date(date));
+  };
+
+  $scope.createMeeting = function() {
+    var date = new Date();
+
+    MeetingFactory.create({
+      author: $scope.me.id,
+      title: date.toLocaleDateString("pt-PT") + " - Meeting",
+      date: date
+    }, function(response) {
+      if (response.success) {
+        $location.path("/meeting/" + response.id + "/edit");
+      }
+    });
   };
 
 });
