b3c16db6c734e84614534260efbe16ec7b4015df
Makay11 <dpais11@gmail.com>
2014-05-08 01:48:03 +0100

    updating a meeting doesn't create new objects

--- serverApp/resources/meeting/update.js
+++ serverApp/resources/meeting/update.js
@@ -6,16 +6,14 @@ function update(request, reply) {
 
   console.log(request.payload);
 
-  var meeting = new Meeting(request.payload);
-
-  meeting.save(function (err){
+  Meeting.update({_id: request.payload._id}, request.payload, function (err){
     if (err) {
       console.log("Error updating meeting!\n" + err);
       reply({error: "There was an error!"});
     }
     else {
       //notification.new(request.auth.credentials.id, 'meeting-'+meeting.id, meeting.name, "meeting",request.auth.credentials.name);
-      console.log("Meeting updated: " + meeting._id);
+      console.log("Meeting updated: " + request.payload._id);
       reply({success: "Meeting updated!"});
     }
   });
