fbbb848b0cfc65c74f020a98ec9df8a805dff6ce
Makay11 <dpais11@gmail.com>
2014-05-20 20:50:37 +0100

    topics/me finished

--- clientApp/js/angularApp/controllers/topic/list.js
+++ clientApp/js/angularApp/controllers/topic/list.js
@@ -1,27 +1,36 @@
 'use strict';
 
-theToolController.controller('TopicsController', function ($scope, $rootScope, $location, TopicFactory) {
+theToolController.controller('TopicsController', function ($scope, $location, $routeParams, TopicFactory) {
 
   //================================INITIALIZATION================================
+
+  if($routeParams.id == "me") {
+    $location.path('/topics/' + $scope.me.id);
+    return;
+  }
 
   $scope.loading = true;
 
   $scope.kinds = ["Info", "To do", "Decision", "Idea"];
 
-  TopicFactory.Topics.getAll(function(topics) {
+  if ($location.path() == '/topics') {
+    TopicFactory.Topics.getAll(gotTopics);
+  }
+  else {
+    TopicFactory.TargetTopics.getAll({id: $scope.me.id}, gotTopics);
+  }
+
+  function gotTopics (topics) {
     $scope.topics = topics;
-
-    console.log($scope.topics);
-    console.log($scope.members);
-
-    $scope.loading = false;
 
     for (var i = 0, j = $scope.topics.length; i < j; i++) {
       $scope.topics[i].facebook = $scope.members.filter(function(o) {
         return $scope.topics[i].author == o.id;
       })[0].facebook;
     }
-  });
+
+    $scope.loading = false;
+  }
 
 
   //===================================FUNCTIONS===================================
