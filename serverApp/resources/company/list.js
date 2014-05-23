b28d29df273ebb9fc03d7eea5aef596b38e18174
Makay11 <dpais11@gmail.com>
2014-05-23 04:15:31 +0100

    clean-up pt. 5

--- serverApp/resources/company/list.js
+++ serverApp/resources/company/list.js
@@ -1,32 +1,16 @@
-var async          = require('async');
-var Company        = require('./../../db/models/company.js');
-var Hapi           = require('hapi');
+var Company = require('./../../db/models/company.js');
 
 module.exports = list;
 
 function list(request, reply) {
 
-  var companies;
-
-  async.series([
-      getCompanies,
-    ], done);
-
-  function getCompanies(cb) {
-    Company.findAll(gotCompanies);
-
-    function gotCompanies(err, result) {
-      if (err) cb(err);
-      companies = result;
-      cb();
+  Company.findAll(function (err, result) {
+    if (!err && result && result.length > 0) {
+      reply(result);
     }
-  }
-
-  function done(err) {
-    if (err) {
-      reply(Hapi.error.badRequest(err.detail));
-    } else {
-      reply(companies);
+    else {
+      reply({error: "Error getting all companies."});
     }
-  }
+  });
+
 }
