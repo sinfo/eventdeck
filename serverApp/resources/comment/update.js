bb135dbbba5ac7119bf24f957c410cfefee8b72d
Makay11 <dpais11@gmail.com>
2014-05-23 03:13:21 +0100

    clean-up pt. 2

--- serverApp/resources/comment/update.js
+++ serverApp/resources/comment/update.js
@@ -1,85 +1,58 @@
-var Hapi           = require('hapi');
-var async          = require('async');
-var Comment        = require('./../../db/models/comment.js');
-var markdown       = require( "markdown" ).markdown;
+var async    = require('async');
+var Comment  = require('./../../db/models/comment.js');
+var markdown = require( "markdown" ).markdown;
 
-exports = module.exports = create;
-
-/// create Comment
+module.exports = create;
 
 function create(request, reply) {
 
-  var commentId = request.params.id;
-  var comment = {};
-  var diffComment = {};
+  var comment = request.payload;
+
+  var savedComment;
 
   async.series([
-      getComment,
-      updateComment,
-      saveComment,
-    ], done);
+    getComment,
+    saveComment
+  ], done);
 
   function getComment(cb) {
-    Comment.findById(commentId, gotComment);
+    Comment.findById(request.params.id, gotComment);
 
     function gotComment(err, result) {
-      if (err) {
-        cb(err);
-      }
-
-      if (result.length > 0) {
-        if (result[0]._id)      { comment._id      = result[0]._id; }
-        if (result[0].thread)   { comment.thread   = result[0].thread; }
-        if (result[0].member)   { comment.member   = result[0].member; }
-        if (result[0].markdown) { comment.markdown = result[0].markdown; }
-
+      if (!err && result && result.length > 0) {
+        savedComment = result[0];
         comment.updated = Date.now();
-    
         cb();
       }
       else {
-        cb(Hapi.error.conflict('No comment with the ID: ' + commentId));
+        cb(err);
       }
     }
-  }
-
-  function updateComment(cb) {
-    console.log(request.payload.member, comment.member, request.payload.member != comment.member)
-
-    if (request.payload.thread   != comment.thread)   { diffComment.thread   = request.payload.thread; }
-    if (request.payload.member   != comment.member)   { diffComment.member   = request.payload.member; }
-    if (request.payload.markdown != comment.markdown) { diffComment.markdown = request.payload.markdown; 
-                                                        diffComment.html     = markdown.toHTML(request.payload.markdown); }
-    if (request.payload.updated  != comment.updated)  { diffComment.updated  = request.payload.updated; }
-
-    console.log("DIFF", diffComment)
-
-    cb();
   }
 
   function saveComment(cb) {
-    if(comment.member != request.auth.credentials.id) {
-      return cb("You're not the author");
+    if (savedComment.member != request.auth.credentials.id) {
+      return cb("You're not the author.");
     }
 
-    var query = {
-      _id: comment._id
-    };
-    Comment.update(query, diffComment, {}, function (err, numAffected){
-      if (err) {
-        return cb(Hapi.error.internal('Hipcup on the DB' + err.detail));
-      }
+    comment.html = markdown.toHTML(request.payload.markdown);
 
-      console.log("UPDATED", numAffected)
-      cb();
+    Comment.update({_id: request.params.id}, comment, function(err) {
+      if (err) {
+        cb(err);
+      }
+      else {
+        cb();
+      }
     });
   }
 
   function done(err) {
     if (err) {
-      reply({error:"There was an error!"});
-    } else {
-      reply({message:"Comment Updated!"});
+      reply({error: "There was an error updating the comment."});
+    }
+    else {
+      reply({success: "Comment updated."});
     }
   }
 }
