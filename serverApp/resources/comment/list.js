0546e69cd1191c3b92355e566fbab7b7a8c2a039
Makay11 <dpais11@gmail.com>
2014-05-23 02:40:34 +0100

    huge clean-up

--- serverApp/resources/comment/list.js
+++ serverApp/resources/comment/list.js
@@ -1,32 +1,16 @@
-var async          = require('async');
-var Comment        = require('./../../db/models/comment.js');
-var Hapi           = require('hapi');
+var Comment = require('./../../db/models/comment.js');
 
 module.exports = list;
 
 function list(request, reply) {
 
-  var comments;
-
-  async.series([
-      getComments,
-    ], done);
-
-  function getComments(cb) {
-    Comment.findAll(gotComments);
-
-    function gotComments(err, result) {
-      if (err) cb(err);
-      comments = result;
-      cb();
+  Comment.findAll(function(err, result) {
+    if (!err && result && result.length > 0) {
+      reply(result);
     }
-  }
-
-  function done(err) {
-    if (err) {
-      reply(Hapi.error.badRequest(err.detail));
-    } else {
-      reply(comments);
+    else {
+      reply({error: "There was an error getting all comments."});
     }
-  }
+  });
+
 }
