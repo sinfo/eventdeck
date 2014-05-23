67fe29f9b12d38fe8489a01453cf700646c05a26
Makay11 <dpais11@gmail.com>
2014-05-24 00:36:37 +0100

    clean-up pt. 7

--- serverApp/resources/meeting/create.js
+++ serverApp/resources/meeting/create.js
@@ -1,23 +1,22 @@
 var Meeting = require('./../../db/models/meeting');
 
-exports = module.exports = create;
+module.exports = create;
 
 function create(request, reply) {
 
   var meeting = new Meeting(request.payload);
 
-  meeting.save(function (err){
+  meeting.save(function (err) {
     if (err) {
-      console.log("Error creating meeting!\n" + err);
-      reply({error: "There was an error!"});
+      reply({error: "There was an error creating the meeting."});
     }
     else {
       //notification.new(request.auth.credentials.id, 'meeting-'+meeting.id, meeting.name, "meeting",request.auth.credentials.name);
-      console.log("Meeting created: " + meeting._id);
       reply({
-        success: "Meeting created!",
+        success: "Meeting created.",
         id: meeting._id
       });
     }
   });
+
 }
--- serverApp/resources/meeting/index.js
+++ serverApp/resources/meeting/index.js
@@ -1,3 +1,5 @@
 exports.list   = require('./list');
+exports.get    = require('./get');
 exports.create = require('./create');
 exports.update = require('./update');
+exports.delete = require('./delete');
