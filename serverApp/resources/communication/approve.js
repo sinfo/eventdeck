a2e6409892bb18cdd9e0a6d329fa9e9c4ba03925
Francisco Baio Dias <xicombd@gmail.com>
2014-05-24 20:22:03 +0100

    communication approve

---
+++ serverApp/resources/communication/approve.js
@@ -0,0 +1,55 @@
+var async    = require('async');
+var Communication  = require('./../../db/models/communication.js');
+
+module.exports = create;
+
+function create(request, reply) {
+
+  var communication = request.payload;
+
+  var savedCommunication;
+
+  async.series([
+    getCommunication,
+    saveCommunication
+  ], done);
+
+  function getCommunication(cb) {
+    Communication.findById(request.params.id, gotCommunication);
+
+    function gotCommunication(err, result) {
+      if (!err && result && result.length > 0) {
+        savedCommunication = result[0];
+        communication.updated = Date.now();
+        cb();
+      }
+      else {
+        cb(err);
+      }
+    }
+  }
+
+  function saveCommunication(cb) {
+    if (savedCommunication.member == request.auth.credentials.id) {
+      return cb("You cannot approve your own stuff.");
+    }
+
+    Communication.update({_id: request.params.id}, {approved: true}, function(err) {
+      if (err) {
+        cb(err);
+      }
+      else {
+        cb();
+      }
+    });
+  }
+
+  function done(err) {
+    if (err) {
+      reply({error: "There was an error approving the communication."});
+    }
+    else {
+      reply({success: "Communication approved."});
+    }
+  }
+}
