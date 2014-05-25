8f1ecad81bae8530bd32e65e448ead2cc8128882
Makay11 <dpais11@gmail.com>
2014-05-25 02:22:58 +0100

    notification (partially) clean

--- serverApp/resources/notification/list.js
+++ serverApp/resources/notification/list.js
@@ -1,14 +1,19 @@
 var Notification = require('./../../db/models/notification.js');
 
-exports = module.exports = list;
+module.exports = list;
 
 function list(request, reply) {
-  Notification.findAll(gotNotifications);
 
-  function gotNotifications(err, result) {
-    if (err)
-      reply(err);
-    else
-      reply(result);
-  }
+  Notification.findAll(function (err, result) {
+    if (err) {
+      reply({error: "There was an error getting all the notifications."});
+    }
+    else if (result && result.length > 0) {
+    	reply(result);
+    }
+    else {
+      reply({error: "There are no notifications."});
+    }
+  });
+
 }
