af64e9cb790f257e30e6c64132d3649e38c300db
Francisco Baio Dias <xicombd@gmail.com>
2014-05-26 00:37:10 +0100

    topic getByMember

--- serverApp/resources/topic/index.js
+++ serverApp/resources/topic/index.js
@@ -1,5 +1,6 @@
-exports.list      = require('./list');
-exports.create    = require('./create');
-exports.get       = require('./get');
-exports.update    = require('./update');
-exports.delete    = require('./delete');
+exports.list        = require('./list');
+exports.create      = require('./create');
+exports.get         = require('./get');
+exports.update      = require('./update');
+exports.delete      = require('./delete');
+exports.getByMember = require('./getByMember')
