d243054104219237a4e412c29d67ae76bc5997c4
Makay11 <dpais11@gmail.com>
2014-05-26 18:05:48 +0100

    comments directive

--- clientApp/js/angularApp/controllers/comment/area.js
+++ clientApp/js/angularApp/controllers/comment/area.js
@@ -1,11 +1,21 @@
-'use strict';
+"use strict";
 
-theToolController.controller('CommentAreaController', function ($scope, $http, $routeParams, CommentFactory) {
+theToolController.controller("CommentAreaController", function ($scope, $http, $routeParams, MemberFactory, CommentFactory) {
 
-  console.log($scope.thread);
+  MemberFactory.Member.getAll(function (members) {
+    $scope.members = members;
+  });
+
+  if ($scope.thread.indexOf("company-") != -1) {
+    CommentFactory.Company.getAll({id: $scope.thread.split("-")[1]}, gotComments);
+  }
+
+  function gotComments(comments) {
+    $scope.comments = comments;
+  }
 
   $scope.quoteComment = function(comment) {
-    $scope.commentData.markdown = '> **'+comment.member+' said**:\n> ' + comment.markdown.split('\n').join('\n> ')+'\n';
+    $scope.commentData.markdown = "> **" + comment.member + " said**:\n> " + comment.markdown.split("\n").join("\n> ") + "\n";
   };
 
   $scope.deleteComment = function(id) {
@@ -18,6 +28,12 @@ theToolController.controller('CommentAreaController', function ($scope, $http, $
         $scope.loading = false;
       });
     });
+  };
+
+  $scope.getMember = function (memberId) {
+    return $scope.members.filter(function(o) {
+      return o.id == memberId;
+    })[0];
   };
 
 });
