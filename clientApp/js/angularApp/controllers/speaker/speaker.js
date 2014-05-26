e3e0da2d780ceda0e61e8066d3d52e893cd57902
Makay11 <dpais11@gmail.com>
2014-05-26 23:17:42 +0100

    comments almost finished

--- clientApp/js/angularApp/controllers/speaker/speaker.js
+++ clientApp/js/angularApp/controllers/speaker/speaker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 theToolController
-  .controller('SpeakerController', function ($scope, $http, $routeParams, $sce, SpeakerFactory, MemberFactory, CommentFactory, NotificationFactory) {
+  .controller('SpeakerController', function ($scope, $http, $routeParams, $sce, SpeakerFactory, MemberFactory, NotificationFactory) {
     $scope.trustSrc = function(src) {
       return $sce.trustAsResourceUrl(src+'#page-body');
     }
@@ -18,42 +18,6 @@ theToolController
       });
     };
 
-    $scope.submitComment = function() {
-      if ($scope.commentData.markdown == ""){
-        $scope.emptyComment = true;
-        return;
-      }
-
-      $scope.loading = true;
-
-      var commentData = this.commentData;
-      commentData.thread = 'speaker-'+this.speaker.id;
-
-      CommentFactory.Comment.create(commentData, function(data) {
-        // if successful, we'll need to refresh the comment list
-        CommentFactory.Speaker.getAll({id: $routeParams.id}, function(getData) {
-          $scope.comments = getData;
-          $scope.loading = false;
-        });
-      });
-    };
-
-    $scope.deleteComment = function(id) {
-      $scope.loading = true;
-
-      CommentFactory.Comment.delete({id: id}, function(data) {
-        // if successful, we'll need to refresh the comment list
-        CommentFactory.Speaker.getAll({id: $routeParams.id}, function(getData) {
-          $scope.comments = getData;
-          $scope.loading = false;
-        });
-      });
-    };
-
-    $scope.quoteComment = function(comment) {
-      $scope.commentData.markdown = '> **'+comment.member+' said**:\n> ' + comment.markdown.split('\n').join('\n> ')+'\n';
-    };
-
     $scope.statuses = ['SUGGESTION','CONTACTED','IN CONVERSATIONS','ACCEPTED','CLOSED DEAL','REJECTED/GIVE UP'];
 
     $scope.speaker = $scope.formData = $scope.getSpeaker($routeParams.id);
@@ -61,22 +25,11 @@ theToolController
     SpeakerFactory.Speaker.get({id: $routeParams.id}, function(response) {
       $scope.speaker = $scope.formData = response;
 
-      CommentFactory.Speaker.getAll({id: $routeParams.id}, function(getData) {
-        $scope.comments = getData;
-        $scope.loading = false;
-      });
-
       NotificationFactory.Speaker.getAll({id: $routeParams.id}, function(getData) {
         $scope.speaker.notifications = getData;
+
+        $scope.loading = false;
       });
     });
 
-    $scope.init = function (){
-      $scope.commentData = {
-        markdown: ""
-      };
-      $scope.emptyComment = false;
-    };
-
-    $scope.init();
   });
