bacd4ab3f0962392f28619e692f1d7aa180259b5
Makay11 <dpais11@gmail.com>
2014-05-26 01:00:39 +0100

    topic clean

--- serverApp/resources/topic/delete.js
+++ serverApp/resources/topic/delete.js
@@ -1,43 +1,28 @@
-var async          = require('async');
-var Topic          = require('./../../db/models/topic.js');
-var Hapi           = require('hapi');
-var Notification   = require('./../../db/models/notification.js');
-var Comment        = require('./../../db/models/comment.js');
+var Topic        = require('./../../db/models/topic.js');
+var Notification = require('./../../db/models/notification.js');
+var Comment      = require('./../../db/models/comment.js');
 
 module.exports = del;
 
 function del(request, reply) {
 
-  var id = request.params.id;
+  var topicId = request.params.id;
 
-  async.series([
-      delTopic,
-    ], done);
-
-  function delTopic(cb) {
-    Topic.del(id, deletedTopic);
-
-    function deletedTopic(err, result) {
-      if (err) cb(err);
-
-      Notification.removeByThread('topic-'+id, function(err, result) {
-        console.log("Notifications removed", result);
-
-      });
-      
-      Comment.removeByThread('topic-'+id, function(err, result) {
-        console.log("Comments removed", result);
-      });
-
-      cb();
-    }
-  }
-
-  function done(err) {
+  Topic.del(topicId, function (err) {
     if (err) {
-      reply(Hapi.error.badRequest(err.detail));
-    } else {
-      reply();
+      reply({error: "There was an error deleting the topic with id '" + topicId + "'."});
     }
-  }
+    else {
+      Notification.removeByThread('topic-'+topicId, function(err, result) {
+        //console.log("Notifications removed", result);
+      });
+
+      Comment.removeByThread('topic-'+topicId, function(err, result) {
+        //console.log("Comments removed", result);
+      });
+
+      reply({success: "Topic deleted."});
+    }
+  });
+
 }
