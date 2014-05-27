3fce1a5231c122fa0637c17d3c0fe23b5c94e6fc
Francisco Baio Dias <xicombd@gmail.com>
2014-05-27 14:04:16 +0100

    role factory

--- clientApp/js/angularApp/services/index.js
+++ clientApp/js/angularApp/services/index.js
@@ -175,6 +175,11 @@ theToolServices
 
   .factory('RoleFactory', function ($resource) {
     return {
-
+      Role: $resource('/api/role/:id', null, {
+        'getAll': {method: 'GET', isArray: true},
+      }),
+      Member: $resource('/api/role/:id/members', null, {
+        'getAll': {method: 'GET', isArray: true}
+      })
     };
   });
