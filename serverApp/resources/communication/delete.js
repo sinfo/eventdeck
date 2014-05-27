f27ee60ba536f002f2f050c2deb137333ec2b008
Francisco Baio Dias <xicombd@gmail.com>
2014-05-27 22:43:59 +0100

    communication delelte server

--- serverApp/resources/communication/delete.js
+++ serverApp/resources/communication/delete.js
@@ -1,16 +1,64 @@
+var async = require('async');
 var Communication = require('./../../db/models/communication.js');
 
 module.exports = remove;
 
-function remove(request, reply) {
+function remove(request, reply) {  
 
-  Communication.del(request.params.id, function(err) {
+  var savedCommunication;
+
+  async.series([
+    getCommunication,
+    checkPermission,
+    deleteCommunication
+  ], done);
+
+  function getCommunication(cb) {
+    Communication.findById(request.params.id, gotCommunication);
+
+    function gotCommunication(err, result) {
+      if (err) {
+        cb(err);
+      }
+      else if (result && result.length > 0) {
+        savedCommunication = result[0];
+        cb();
+      }
+      else {
+        cb('Could not find the communication.');
+      }
+    }
+  }
+
+  function checkPermission(cb) {
+    var roles = request.auth.credentials.roles.filter(function(o) {
+      return o.id == 'development-team' || o.id == 'coordination';
+    });
+
+    if(roles.length == 0 && savedCommunication.member != request.auth.credentials.id) {
+      return cb('You don\'t have permissions for this.');
+    }
+    
+    cb();
+  }
+
+  function deleteCommunication(cb) {
+    Communication.del(request.params.id, function(err) {
+      if (err) {
+        cb('Error on the database');
+      }
+      else {
+        cb();
+      }
+    });
+  }
+
+  function done(err) {
     if (err) {
-      reply({error: "There was an error deleting the communication."});
+      reply({error: err});
     }
     else {
-      reply({success: "Communication deleted."});
+      reply({success: 'Communication deleted.'});
     }
-  });
-
+  }
 }
