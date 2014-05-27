61eedc1b2b0a26320dd9a1121956c3763d265527
Makay11 <dpais11@gmail.com>
2014-05-27 23:57:21 +0100

    topics almost finished

--- clientApp/js/angularApp/controllers/topic/embed.js
+++ clientApp/js/angularApp/controllers/topic/embed.js
@@ -1,6 +1,6 @@
 "use strict";
 
-theToolController.controller("TopicEmbedController", function ($scope) {
+theToolController.controller("TopicEmbedController", function ($scope, TopicFactory) {
 
   //================================INITIALIZATION================================
 
@@ -12,12 +12,16 @@ theToolController.controller("TopicEmbedController", function ($scope) {
 
   $scope.pollKinds = ['text', 'images'];
 
-  kind($scope.topic);
+  $scope.topic = JSON.parse($scope.topicJson);
+
+  $scope.members = JSON.parse($scope.membersJson);
+
+  show($scope.topic);
 
 
   //=================================AUXFUNCTIONS==================================
 
-  function kind(topic) {
+  function show(topic) {
     topic.show = {
       text     : true,
       targets  : true,
@@ -25,22 +29,19 @@ theToolController.controller("TopicEmbedController", function ($scope) {
       duedate  : false,
       meeting  : true,
       closed   : false
-    }
-    if(topic.kind === 'To do'){
+    };
+
+    if (topic.kind === 'To do') {
       topic.show.duedate = true;
       topic.show.closed  = true;
     }
-    else if(topic.kind === 'Decision'){
+    else if (topic.kind === 'Decision') {
       topic.show.duedate = true;
       topic.show.closed  = true;
       topic.show.poll = true;
     }
 
     $scope.loading = false;
-
-    setTimeout(function () {
-      console.log($scope.topic, $scope.topic['kind']);
-    }, 5000);
   }
 
 
@@ -137,6 +138,72 @@ theToolController.controller("TopicEmbedController", function ($scope) {
         $scope.error = "There was an error. Please contact the Dev Team and give them the details about the error.";
       } else if (response.success) {
         $scope.success = response.success;
+      }
+    });
+  };
+
+  $scope.getMember = function (memberId) {
+    return $scope.members.filter(function(o) {
+      return o.id == memberId;
+    })[0];
+  };
+
+  $scope.toggleTarget = function(target) {
+    var index = $scope.topic.targets.indexOf(target);
+
+    if (index == -1) {
+      $scope.topic.targets.push(target);
+    }
+    else {
+      $scope.topic.targets.splice(index, 1);
+    }
+  };
+
+  $scope.toggleAllTargets = function() {
+    for (var i = 0, j = $scope.members.length; i < j; i++) {
+      $scope.toggleTarget($scope.members[i].id);
+    }
+  };
+
+  $scope.toggleRoleTargets = function(roleId) {
+    for (var i = 0, j = $scope.members.length; i < j; i++) {
+      for(var o = 0; o < $scope.members[i].roles.length; o++) {
+        if($scope.members[i].roles[o].id == roleId) {
+          $scope.toggleTarget($scope.members[i].id);
+        }
+      }
+    }
+  };
+
+  $scope.toggleTargets = function() {
+    console.log($scope.showTargets);
+    $scope.showTargets = !$scope.showTargets;
+  };
+
+  $scope.focusOption = function(option) {
+    for (var i = 0, j = $scope.topic.poll.options.length; i < j; i++) {
+      $scope.topic.poll.options[i].editing = false;
+    }
+
+    option.editing = true;
+  };
+
+  $scope.selectOption = function(topic, option) {
+    var updatedTopic = topic;
+
+    if(option.votes.indexOf($scope.me.id) != -1) {
+      updatedTopic.poll.options[updatedTopic.poll.options.indexOf(option)].votes.splice(updatedTopic.poll.options[updatedTopic.poll.options.indexOf(option)].votes.indexOf($scope.me.id),1);
+    }
+    else {
+      updatedTopic.poll.options[updatedTopic.poll.options.indexOf(option)].votes.push($scope.me.id);
+    }
+
+    TopicFactory.Topic.update({id: updatedTopic._id}, updatedTopic, function(response) {
+      if(response.error) {
+        console.log("There was an error. Please contact the Dev Team and give them the details about the error.");
+      }
+      else if (response.success) {
+        console.log(response.success);
       }
     });
   };
