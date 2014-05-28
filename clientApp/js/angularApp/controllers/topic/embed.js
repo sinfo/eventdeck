a9d158e44bb6d9420c917a26ea63b88c4b4b22a8
Francisco Baio Dias <xicombd@gmail.com>
2014-05-28 03:37:51 +0100

    sooo beautiful

--- clientApp/js/angularApp/controllers/topic/embed.js
+++ clientApp/js/angularApp/controllers/topic/embed.js
@@ -223,4 +223,38 @@ theToolController.controller("TopicEmbedController", function ($scope, TopicFact
     });
   };
 
+  $scope.timeSince =function (date) {
+    date = new Date(date);
+    var seconds = Math.floor((Date.now() - date) / 1000);
+
+    var suffix = 'ago';
+    if(seconds < 0){
+      seconds = Math.abs(seconds);
+      suffix = 'to go';
+    }
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years " + suffix;
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months " + suffix;
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days " + suffix;
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours " + suffix;
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes " + suffix;
+    }
+    return Math.floor(seconds) + " seconds " + suffix;
+  };
+
 });
