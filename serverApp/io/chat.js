ec598f0667ebf5e3fd9ac6304c43f92145c69fdc
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-05-28 19:22:35 +0100

    chat connection working

--- serverApp/io/chat.js
+++ serverApp/io/chat.js
@@ -1,11 +1,48 @@
 var webSocket = require('./../index.js').webSocket;
+var Chat = require('./../resources/chat');
+var Message = require('./../resources/message');
 
 webSocket
-	.of('/chat')
-	.on('connection', function (socket) {
-		console.log("Connected to main chat!");
-		/*socket.emit('init', { message: 'welcome to the chat' });*/
-	    socket.on('auth', function(data){
-	    	socket.join(data.id);
-	    });
-  	});
+  .of('/chat')
+  .on('connection', function (socket) {
+    console.log("Connected to main chat!");
+    /*socket.emit('init', { message: 'welcome to the chat' });*/
+      socket.on('auth', function(data, cb){
+        console.log("Chat ID: " + data.id);
+
+    var room = data.id;
+
+    async.series([
+      getMessage,
+    ], done);
+
+    Chat.get({id: room}, function(response) {
+      if(response.error) {
+        console.log('Chat id: ' + room + ' unavailable');
+      } 
+      else {
+        console.log('Logged in chat id: ' + room );
+        socket.join(room);
+      } 
+    });
+
+    Message.getByChatId({id: room}, function(response){
+      if(response.error) {
+        console.log('Chat id: ' + room + ' messages unavailable');
+      } 
+      else {
+        console.log('Got messages in chat id: ' + room );
+        socket.join(room);
+      } 
+    }); 
+
+
+
+
+    cb();
+      });
+    });
+
+function getChat(chatID){
+
+}
