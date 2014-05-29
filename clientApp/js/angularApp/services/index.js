f1a873865c50b0e2fb41783a4bcf20a7006dcd49
Francisco Baio Dias <xicombd@gmail.com>
2014-05-29 01:27:49 +0100

    tag factory

--- clientApp/js/angularApp/services/index.js
+++ clientApp/js/angularApp/services/index.js
@@ -184,6 +184,20 @@ theToolServices
     };
   })
 
+  .factory('TagFactory', function ($resource) {
+    return {
+      Tag: $resource('/api/tag/:id', null, {
+        'getAll': {method: 'GET', isArray: true},
+        'update': {method: 'PUT'},
+        'create': {method: 'POST'},
+        'delete': {method: 'DELETE'}
+      }),
+      Topic: $resource('/api/tag/:id/topics', null, {
+        'getAll': {method: 'GET', isArray: true}
+      })
+    };
+  })
+
   .factory('SocketFactory', function ($resource, $location, $rootScope) {
     var socket = io.connect('/chat');
     return {
