5daaaf8bf4e42ddaa45474768270f69fed16604e
Francisco Baio Dias <xicombd@gmail.com>
2014-05-30 00:56:07 +0100

    forgotten tag stuff

---
+++ clientApp/js/angularApp/controllers/tag/manager.js
@@ -0,0 +1,38 @@
+"use strict";
+
+theToolController.controller("TagManagerController", function ($scope, TagFactory) {
+
+  $scope.loading = true;
+
+  $scope.newTag = {};
+
+  $scope.lightColors = ['#f7c6c7', '#fad8c7', '#fef2c0', '#bfe5bf', '#bfdadc', '#c7def8', '#bfd4f2', '#d4c5f9'];
+  $scope.colors = ['#e11d21', '#eb6420', '#fbca04', '#009800', '#006b75', '#207de5', '#0052cc', '#5319e7'];
+
+  $scope.changeColor = function(color) {
+    $scope.newTag.color = color;
+    console.log("COLOR", color);
+  }
+
+  $scope.createTag = function(newTag) {
+    console.log("CREATE", newTag);
+
+    TagFactory.Tag.create(newTag, function(response) {
+      if(response.success) {
+        $scope.tags.push(response.tag);
+      }
+    });
+  }
+
+  $scope.deleteTag = function(tag) {
+    console.log("DELETE", tag);
+
+    TagFactory.Tag.delete({id:tag.id}, function(response) {
+      console.log("RESPONSE", response);
+      if(response.success) {
+        $scope.tags.splice(tag.id, 1);
+      }
+    });
+  }
+
+});
