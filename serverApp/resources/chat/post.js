47dd2b7a19ea14d4f79175e9d075a82c6908f1cb
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-05-30 03:03:59 +0100

    chat working \o/

--- serverApp/resources/chat/post.js
+++ serverApp/resources/chat/post.js
@@ -27,7 +27,6 @@ function update(request, reply) {
       }
 
       if (result.length > 0) {
-        console.log(result[0]);
         if (result[0].id)            { chat = result[0]; }
         cb();
       }
