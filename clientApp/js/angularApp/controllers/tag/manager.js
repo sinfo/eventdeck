2c967e7e69de3ab2836ebf93a7c35c587f54ca2f
Makay11 <dpais11@gmail.com>
2014-05-31 01:09:05 +0100

    manage tags

--- clientApp/js/angularApp/controllers/tag/manager.js
+++ clientApp/js/angularApp/controllers/tag/manager.js
@@ -6,34 +6,30 @@ theToolController.controller("TagManagerController", function ($scope, TagFactor
 
   $scope.newTag = {};
 
-  $scope.lightColors = ['#f7c6c7', '#fad8c7', '#fef2c0', '#bfe5bf', '#bfdadc', '#c7def8', '#bfd4f2', '#d4c5f9'];
-  $scope.colors = ['#e11d21', '#eb6420', '#fbca04', '#009800', '#006b75', '#207de5', '#0052cc', '#5319e7'];
+  $scope.lightColors = ["#f7c6c7", "#fad8c7", "#fef2c0", "#bfe5bf", "#bfdadc", "#c7def8", "#bfd4f2", "#d4c5f9"];
+  $scope.colors = ["#e11d21", "#eb6420", "#fbca04", "#009800", "#006b75", "#207de5", "#0052cc", "#5319e7"];
 
-  $scope.changeColor = function(color) {
+  $scope.changeColor = function (color) {
     $scope.newTag.color = color;
-    console.log("COLOR", color);
-  }
+  };
 
-  $scope.createTag = function(newTag) {
-    console.log("CREATE", newTag);
+  $scope.createTag = function (newTag) {
 
-    TagFactory.Tag.create(newTag, function(response) {
-      if(response.success) {
+    TagFactory.Tag.create(newTag, function (response) {
+      if (response.success) {
         $scope.tags.push(response.tag);
       }
     });
-  }
+  };
 
-  $scope.deleteTag = function(tag) {
-    console.log("DELETE", tag);
+  $scope.deleteTag = function (tag) {
 
-    TagFactory.Tag.delete({id:tag.id}, function(response) {
-      console.log("RESPONSE", response);
-      if(response.success) {
+    TagFactory.Tag.delete({id: tag.id}, function (response) {
+      if (response.success) {
         var index = $scope.tags.indexOf(tag);
         $scope.tags.splice(index, 1);
       }
     });
-  }
+  };
 
 });
