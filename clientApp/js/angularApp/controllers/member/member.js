3ff3189912bfe5088346e5dca381eac0f742d03b
Makay11 <dpais11@gmail.com>
2014-05-31 20:38:15 +0100

    slight tweaks

--- clientApp/js/angularApp/controllers/member/member.js
+++ clientApp/js/angularApp/controllers/member/member.js
@@ -1,48 +1,47 @@
-'use strict';
+"use strict";
 
-theToolController
-  .controller('MemberController', function ($scope, $http, $routeParams, $sce, $location, MemberFactory) {
-    $scope.getClassFromKind = function(participation) {
-      if(!participation) { return "nope"; }
-      if(!participation.kind) { return "sponsor"; }
-      var kind = participation.kind.toLowerCase();
+theToolController.controller("MemberController", function ($scope, $http, $routeParams, $sce, $location, MemberFactory) {
+  $scope.getClassFromKind = function(participation) {
+    if(!participation) { return "nope"; }
+    if(!participation.kind) { return "sponsor"; }
+    var kind = participation.kind.toLowerCase();
 
-      if(kind.indexOf("bronze") != -1) { return "bronze"; }
-      else if(kind.indexOf("prata") != -1) { return "silver"; }
-      else { return "sponsor"; }
-    }
+    if(kind.indexOf("bronze") != -1) { return "bronze"; }
+    else if(kind.indexOf("prata") != -1) { return "silver"; }
+    else { return "sponsor"; }
+  }
 
-    $scope.loading = true;
+  $scope.loading = true;
 
-    MemberFactory.Member.get({id: $routeParams.id}, function(response) {
-      if($routeParams.id == "me") {
-        var newPath = "/member/"+response.id;
-        $location.path(newPath);
-      }
-      $scope.member = response;
+  if ($routeParams.id === "me") {
+    $location.path("/member/" + $scope.me.id);
+    return;
+  }
 
-      getMemberStuff();
-    });
-
-
-    function getMemberStuff() {
-      $scope.member.companies = $scope.companies.filter(function(e) {
-        return e.member == $scope.member.id;
-      })
-
-      $scope.member.speakers = $scope.speakers.filter(function(e) {
-        return e.member == $scope.member.id;
-      })
-
-      $scope.member.comments = $scope.comments.filter(function(e) {
-        return e.member == $scope.member.id;
-      })
-
-      if($scope.companies.length > 0 && $scope.speakers.length > 0) {
-        $scope.loading = false;
-      } else {
-        setTimeout(getMemberStuff, 1000);
-      }
-    }
-
+  $scope.member = $scope.members.find(function (o) {
+    return o.id === $routeParams.id;
   });
+
+  getMemberStuff();
+
+  function getMemberStuff() {
+    $scope.member.companies = $scope.companies.filter(function(e) {
+      return e.member == $scope.member.id;
+    })
+
+    $scope.member.speakers = $scope.speakers.filter(function(e) {
+      return e.member == $scope.member.id;
+    })
+
+    $scope.member.comments = $scope.comments.filter(function(e) {
+      return e.member == $scope.member.id;
+    })
+
+    if($scope.companies.length > 0 && $scope.speakers.length > 0) {
+      $scope.loading = false;
+    } else {
+      setTimeout(getMemberStuff, 1000);
+    }
+  }
+
+});
