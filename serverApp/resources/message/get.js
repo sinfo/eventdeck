b07f6fdb2835d213111861c65519516eb7b83479
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-05-31 01:56:53 +0100

    clean back end chat

--- serverApp/resources/message/get.js
+++ serverApp/resources/message/get.js
@@ -11,9 +11,7 @@ function get(request, reply) {
   var messageId = request.params.id;
   var message   = {};
 
-  async.series([
-      getMessage,
-    ], done);
+  getMessage(done);
 
   function getMessage(cb) {
     Message.findById(messageId, gotMessage);
@@ -24,11 +22,7 @@ function get(request, reply) {
       }
 
       if (result.length > 0) {
-        if (result[0].id)            { message.id            = result[0].id; }
-        if (result[0].chatId)        { message.chatId        = result[0].chatId; }
-        if (result[0].member)        { message.member        = result[0].member; }
-        if (result[0].text)          { message.text          = result[0].text; }
-        if (result[0].date)          { message.date          = result[0].date; }
+        message.id = result[0].id;
         cb();
       }
       else {
