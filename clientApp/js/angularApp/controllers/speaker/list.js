2d23c03d83abf606bc3256f3a9575501678f528a
Francisco Baio Dias <xicombd@gmail.com>
2014-06-01 00:10:11 +0100

    speakers & companies

--- clientApp/js/angularApp/controllers/speaker/list.js
+++ clientApp/js/angularApp/controllers/speaker/list.js
@@ -2,26 +2,20 @@
 
 theToolController
   .controller('SpeakersController', function ($scope, $http, $sce, SpeakerFactory, MemberFactory) {
-    $scope.trustSrc = function(src) {
-      return $sce.trustAsResourceUrl(src);
-    }
+  
+    $scope.limit = 10;
 
-    $scope.getClassFromKind = function(status) {
-      if(!status) { return "suggestion"; }
-      var kind = status.toLowerCase();
-
-      if(kind.indexOf("contactado") != -1) { return "contacted"; } 
-      else if(kind.indexOf("aceitou") != -1) { return "accepted"; }
-      else if(kind.indexOf("rejeitou") != -1) { return "rejected"; }
-      else if(kind.indexOf("conversações") != -1) { return "conversations"; }
-      else if(kind.indexOf("desistir") != -1) { return "giveup"; }
-      else { return "suggestion"; }
-    }
-
+    $scope.statuses = ['Suggestion','Contacted','In Conversations','Accepted','Rejected','Give Up'];
+    
     SpeakerFactory.Speaker.getAll(function(response) {
       $scope.predicate = 'participation';
       $scope.reverse = false;
       $scope.speakers = response;
     });
+
+    $scope.scroll = function() {
+      if ($scope.limit <= $scope.speakers.length)
+        $scope.limit += 10;
+    };
   });
   
