44f1e27174d95ecf4039c89bc5747bec9d534053
Francisco Baio Dias <xicombd@gmail.com>
2014-06-01 18:56:49 +0100

    memmber list

--- clientApp/js/angularApp/filters/index.js
+++ clientApp/js/angularApp/filters/index.js
@@ -1,9 +1,21 @@
 'use strict';
- 
- 
-angular.module('theTool.filters', []).
-  filter('interpolate', ['version', function(version) {
+
+angular.module('theTool.filters', [])
+  .filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]);
+  }])
+  .filter('filterRole', function() {
+    return function(members, role) {
+          var result = members;
+          if(role) {
+            result = members.filter(function(m) {
+              return m.roles.filter(function(r) {
+                return r.id == role;
+              }).length > 0;
+            });
+          }
+          return result;
+      };
+  });
