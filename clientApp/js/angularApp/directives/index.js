9399896aa0d9c9d285d84b09b4a2aa8bae106770
Makay11 <dpais11@gmail.com>
2014-06-02 18:18:22 +0100

    embed meeting

--- clientApp/js/angularApp/directives/index.js
+++ clientApp/js/angularApp/directives/index.js
@@ -1,10 +1,11 @@
-theToolDirectives = angular.module('theTool.directives', []);
+theToolDirectives = angular.module("theTool.directives", []);
 
-require('./comment');
-require('./communication');
-require('./company');
-require('./date');
-require('./markdown');
-require('./speaker');
-require('./tag');
-require('./topic');
+require("./comment");
+require("./communication");
+require("./company");
+require("./date");
+require("./markdown");
+require("./meeting");
+require("./speaker");
+require("./tag");
+require("./topic");
