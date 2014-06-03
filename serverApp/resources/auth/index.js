9e574f7311bf4c55fb1fcaa0935412d18c11c5f0
Makay11 <dpais11@gmail.com>
2014-06-03 19:27:49 +0100

    code refactoring and member creation with facebook id retrieval

--- serverApp/resources/auth/index.js
+++ serverApp/resources/auth/index.js
@@ -1,18 +1,18 @@
 var Hapi           = require("hapi");
 var fenix          = require("fenixedu");
-var Request        = require("request");
 var Member         = require("./../../db/models/member.js");
+var Request        = require("request");
 var facebookConfig = require("./facebookConfig.js");
 
-exports = module.exports;
+var exports = module.exports;
 
 exports.login = function login(request, reply) {
-
   if (request.auth.isAuthenticated) {
-    return reply().redirect("/");
+    reply().redirect("/");
   }
-
-  return reply.view("login.html", { url: fenix.auth.getAuthUrl() });
+  else {
+    reply.view("login.html", { url: fenix.auth.getAuthUrl() });
+  }
 };
 
 exports.facebook = function facebook(request, reply) {
@@ -46,31 +46,36 @@ exports.facebook = function facebook(request, reply) {
 }
 
 exports.redirect = function redirect(request, reply) {
-
   if (request.auth.isAuthenticated) {
     return reply().redirect("/");
   }
 
-  var account = null;
+  if (request.url.query.error) {
+    return reply.view("error.html", { error: request.url.query.error_description });
+  }
 
-  if(request.url.query.error) { return reply.view("error.html", { error: request.url.query.error_description }); }
-
-  fenix.auth.getAccessToken(request.url.query.code, function(error, body) {
-    if (error) { return reply.view("error.html"); }
+  fenix.auth.getAccessToken(request.url.query.code, function (error, body) {
+    if (error) {
+      return reply.view("error.html");
+    }
 
     var refresh_token = body.refresh_token;
     var access_token = body.access_token;
 
-    fenix.person.getPerson(access_token, function(error, person) {
-      if (error) { return reply.view("error.html", { error: error.error_description }); }
+    fenix.person.getPerson(access_token, function (error, person) {
+      if (error) {
+        return reply.view("error.html", { error: error.error_description });
+      }
 
       var person = JSON.parse(person);
 
-      Member.findByIstId(person.username, function(error, result) {
-        if (error) { return reply.view("error.html"); }
+      Member.findByIstId(person.username, function (error, result) {
+        if (error) {
+          return reply.view("error.html");
+        }
 
         if (result.length > 0) {
-          account = result[0];
+          var account = result[0];
 
           console.log("FENIX LOG IN", account);
 
@@ -78,7 +83,7 @@ exports.redirect = function redirect(request, reply) {
           return reply().redirect("/");
         }
         else {
-          return reply.view("error.html", { error: "Your ist id is not allowed :-(" });
+          return reply.view("error.html", { error: "Your ist id is not allowed." });
         }
       });
     });
@@ -86,7 +91,6 @@ exports.redirect = function redirect(request, reply) {
 };
 
 exports.logout = function logout(request, reply) {
-
   request.auth.session.clear();
-  return reply().redirect("/");
+  reply().redirect("/");
 };
--- serverApp/scripts/createUsersFromDocs.js
+++ serverApp/scripts/createUsersFromDocs.js
@@ -1,40 +1,40 @@
-var Tabletop = require('tabletop');
-var Request  = require('request');
-var async    = require('async');
-var Member   = require('./../db/models/member.js');
-require('./../db');
+var Tabletop = require("tabletop");
+var Request  = require("request");
+var async    = require("async");
+var Member = require("./../db/models/member.js");
+require("./../db");
 
 var sheetData  = [];
 var commission = [];
 
-var KEY = '0ArFdIIlHHZzpdEhaSXp5WktSdzR0Z2hLWkNibkd3dHc';
+var KEY = "0ArFdIIlHHZzpdEhaSXp5WktSdzR0Z2hLWkNibkd3dHc";
 
 Tabletop.init({
   key: KEY,
-  callback: function(data, tabletop) {
+  callback: function (data, tabletop) {
     sheetData = data;
     commission = sheetData["Comissão"].elements;
 
     array = [];
-    for (var i = 0; i < commission.length; i++){
+    for (var i = 0; i < commission.length; i++) {
       array.push(i);
     }
 
-    async.eachSeries(array, function(i, cb) {
+    async.eachSeries(array, function (i, cb) {
       var member = {};
 
       if(commission[i].mailsinfo) {
-        member.id = commission[i].mailsinfo.split('@')[0];
+        member.id = commission[i].mailsinfo.split("@")[0];
         if (commission[i].nome)   { member.name  = commission[i].nome;          }
-        if (commission[i].naluno) { member.istId = 'ist1'+commission[i].naluno; }
+        if (commission[i].naluno) { member.istId = "ist1"+commission[i].naluno; }
         if (commission[i].cargo)  {
           var roles = [];
-          for(var r in commission[i].cargo.split('/')) {
-            r = commission[i].cargo.split('/')[r];
+          for(var r in commission[i].cargo.split("/")) {
+            r = commission[i].cargo.split("/")[r];
             var role = {
-              id: createId(r.replace(' (TL)', '')),
-              name: r.replace(' (TL)', ''),
-              isTeamLeader: r.indexOf('(TL)') != -1 || r.indexOf('Manager') != -1
+              id: createId(r.replace(" (TL)", "")),
+              name: r.replace(" (TL)", ""),
+              isTeamLeader: r.indexOf("(TL)") != -1 || r.indexOf("Manager") != -1
             };
             roles.push(role);
           }
@@ -42,6 +42,7 @@ Tabletop.init({
         }
         if (commission[i].facebook)    { member.facebook      = commission[i].facebook;    }
         if (commission[i].skype)       { member.skype         = commission[i].skype;       }
+
         member.mails = {};
         if (commission[i].mailsinfo)   { member.mails.sinfo   = commission[i].mailsinfo;   }
         if (commission[i].mailist)     { member.mails.ist     = commission[i].mailist;     }
@@ -49,37 +50,42 @@ Tabletop.init({
         if (commission[i].mailgoogle)  { member.mails.google  = commission[i].mailgoogle;  }
         if (commission[i].mailmsn)     { member.mails.msn     = commission[i].mailmsn;     }
         if (commission[i].maildropbox) { member.mails.dropbox = commission[i].maildropbox; }
+
         member.phones = [];
-        if (commission[i]['telem.1'])  { member.phones.push(commission[i]['telem.1']);     }
-        if (commission[i]['telem.2'])  { member.phones.push(commission[i]['telem.2']);     }
+        if (commission[i]["telem.1"])  { member.phones.push(commission[i]["telem.1"]);     }
+        if (commission[i]["telem.2"])  { member.phones.push(commission[i]["telem.2"]);     }
 
         Request("http://graph.facebook.com/" + member.facebook, {
           method: "GET",
           json: true
-        }, function (error, response, result) {
+        },
+        function (error, response, result) {
           if (!error && response.statusCode == 200) {
             member.facebookId = result.id;
 
             var newMember = new Member(member);
 
-            newMember.save(function (err, reply){
-              if (err)
+            newMember.save(function (err, reply) {
+              if (err) {
                 console.log("ERROR", err, reply);
-              else
+              }
+              else {
                 console.log("SUCCESS", reply.name);
+              }
 
               cb();
             });
           }
         });
       }
-    }, function(err) {
-
+    },
+    function (err) {
+      // do nothing
     });
   },
   simpleSheet: false
 });
 
 function createId(text) {
-  return text.toLowerCase().replace(/ç/g, 'c').replace(/á|à|ã/g, 'a').replace(/é|è|ê/g, 'e').replace(/í|ì|î/g, 'i').replace(/ó|ò|õ|ô/g, 'o').replace(/[^a-zA-Z ]/g, '').replace(/\s/g, '-');
+  return text.toLowerCase().replace(/ç/g, "c").replace(/á|à|ã/g, "a").replace(/é|è|ê/g, "e").replace(/í|ì|î/g, "i").replace(/ó|ò|õ|ô/g, "o").replace(/[^a-zA-Z ]/g, "").replace(/\s/g, "-");
 }
