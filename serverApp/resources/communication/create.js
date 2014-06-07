2c5fe1f997ebb892fb119f71ab32b530196610ec
Francisco Baio Dias <xicombd@gmail.com>
2014-06-07 01:05:30 +0100

    getTargets

--- serverApp/resources/communication/create.js
+++ serverApp/resources/communication/create.js
@@ -1,5 +1,6 @@
 var Communication = require('./../../db/models/communication.js');
 var notification  = require('./../notification');
+var getTargets   = require('./../member').getTargetsByThread;
 
 module.exports = create;
 
@@ -16,7 +17,11 @@ function create(request, reply) {
       reply({error: "Error creating communication."});
     }
     else {
-      notification.notify(communication.member, communication.thread, 'posted a new communication', newCommunication._id);
+      getTargets(comment.thread, function(err, targets) {
+        if(err) { console.log(err); }
+
+        notification.notify(communication.member, communication.thread, 'posted a new communication', newCommunication._id, targets);
+      });
 
       reply({success: "Communication created."});
     }
