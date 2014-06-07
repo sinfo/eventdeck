6837c505fc5b20a99589a2b25527e9846f339d54
Francisco Baio Dias <xicombd@gmail.com>
2014-06-07 01:05:30 +0100

    getTargets

--- serverApp/resources/comment/create.js
+++ serverApp/resources/comment/create.js
@@ -2,6 +2,7 @@ var Comment      = require('./../../db/models/comment.js');
 var markdown     = require('markdown').markdown;
 var email        = require('./../email');
 var notification = require('./../notification');
+var getTargets   = require('./../member').getTargetsByThread;
 
 module.exports = create;
 
@@ -21,8 +22,12 @@ function create(request, reply) {
       reply({error: "Error creating comment."});
     }
     else {
-      email.comment(comment);
-      notification.notify(comment.member, comment.thread, 'posted a new comment', newComment._id);
+      getTargets(comment.thread, function(err, targets) {
+        if(err) { console.log(err); }
+
+        email.comment(comment);
+        notification.notify(comment.member, comment.thread, 'posted a new comment', newComment._id, targets);
+      });
 
       reply({success: "Comment created."});
     }
