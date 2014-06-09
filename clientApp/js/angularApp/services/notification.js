a3d35741176c582f727c19052150dd66a38066a2
Diogo Pais <dpais11@gmail.com>
2014-06-09 15:11:03 +0100

    reading topic notifications on mouse-over

--- clientApp/js/angularApp/services/notification.js
+++ clientApp/js/angularApp/services/notification.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
-theToolServices
-  .factory('NotificationFactory', function ($resource) {
-    return {
-      Notification: $resource('/api/notification/:id', null, {
-        'getAll': {method: 'GET', isArray: true},
-      }),
-      Company: $resource('/api/company/:id/notifications', null, {
-        'getAll': {method: 'GET', isArray: true}
-      }),
-      Speaker: $resource('/api/speaker/:id/notifications', null, {
-        'getAll': {method: 'GET', isArray: true}
-      }),
-      Topic: $resource('/api/topic/:id/notifications', null, {
-        'getAll': {method: 'GET', isArray: true}
-      })
-    }
-  })
+theToolServices.factory("NotificationFactory", function ($resource) {
+  return {
+    Notification: $resource("/api/notification/:id", null, {
+      "getAll": {method: "GET", isArray: true},
+      "update": {method: "PUT"}
+    }),
+    Company: $resource("/api/company/:id/notifications", null, {
+      "getAll": {method: "GET", isArray: true}
+    }),
+    Speaker: $resource("/api/speaker/:id/notifications", null, {
+      "getAll": {method: "GET", isArray: true}
+    }),
+    Topic: $resource("/api/topic/:id/notifications", null, {
+      "getAll": {method: "GET", isArray: true}
+    })
+  };
+});
