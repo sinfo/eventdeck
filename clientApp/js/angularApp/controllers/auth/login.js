b0061f6ed8362207ff465b4c5ed6789865ebd573
Diogo Pais <dpais11@gmail.com>
2014-06-11 18:10:51 +0100

    static login page

---
+++ clientApp/js/angularApp/controllers/auth/login.js
@@ -0,0 +1,21 @@
+"use strict";
+
+theToolController.controller("LoginController", function ($scope) {
+
+  //================================INITIALIZATION================================
+
+
+
+
+  //=================================AUXFUNCTIONS==================================
+
+
+
+
+  //===================================FUNCTIONS===================================
+
+  $scope.login = function () {
+    alert("Heil Makay!");
+  };
+
+});
