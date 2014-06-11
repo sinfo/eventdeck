a0b3962d0012d081bc78e1002f382028ea85b3d1
Diogo Pais <dpais11@gmail.com>
2014-06-12 00:15:18 +0100

    working on the login

--- clientApp/js/angularApp/controllers/auth/login.js
+++ clientApp/js/angularApp/controllers/auth/login.js
@@ -1,10 +1,16 @@
 "use strict";
 
-theToolController.controller("LoginController", function ($scope) {
+theToolController.controller("LoginController", function ($scope, $location) {
 
   //================================INITIALIZATION================================
 
+  $.ajaxSetup({cache: true});
+  $.getScript("//connect.facebook.net/pt_PT/all.js", function () {
+    FB.init({appId: "457207507744159"});
+  });
 
+  var lock = false;
+  var redirecting = false;
 
 
   //=================================AUXFUNCTIONS==================================
@@ -15,7 +21,74 @@ theToolController.controller("LoginController", function ($scope) {
   //===================================FUNCTIONS===================================
 
   $scope.login = function () {
-    alert("Heil Makay!");
+  	if (lock) {
+      return;
+    }
+
+    lock = true;
+
+    FB.getLoginStatus(function (response) {
+      if (response.status === "connected") {
+        connected(response);
+      }
+      else {
+        FB.login();
+        FB.Event.subscribe("auth.statusChange", function (response) {
+          if (response.status === "connected") {
+            connected(response);
+          }
+        });
+
+        lock = false;
+      }
+    });
+
+    function connected(response) {
+      var loginInfo = $("#login");
+
+      loginInfo.find("p:eq(0)").text("Logging in...");
+      loginInfo.find("p:eq(1)").text("");
+      loginInfo.find("i").show();
+
+      loginInfo.show();
+
+      $.ajax(location.origin + "/login/facebook", {
+        type: "GET",
+        data: {
+          id: response.authResponse.userID,
+          token: response.authResponse.accessToken
+        },
+        complete: function (response, status) {
+          if (redirecting) {
+            return;
+          }
+
+          console.log(response);
+
+          if (status !== "success") {
+            loginInfo.find("p:eq(0)").text("There was an error with your request.");
+            loginInfo.find("p:eq(1)").text("Please try again.");
+            loginInfo.find("i").hide();
+
+            lock = false;
+          }
+          else if (response.responseJSON.success) {
+            redirecting = true;
+
+            loginInfo.find("p:eq(0)").text("Success!");
+            loginInfo.find("p:eq(1)").text("Redirecting...");
+
+            //$location.path("/");
+          }
+          else {
+            loginInfo.find("p:eq(0)").text("You are not authorized to log in.");
+            loginInfo.find("i").hide();
+
+            lock = false;
+          }
+        }
+      });
+    }
   };
 
 });
