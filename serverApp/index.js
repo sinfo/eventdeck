834ce0ab6c61c619f0238d9086a62707de348b8c
Diogo Pais <dpais11@gmail.com>
2014-06-25 11:42:49 +0100

    Revert "All angular"

--- serverApp/index.js
+++ serverApp/index.js
@@ -4,7 +4,7 @@ var options      = require("./options");
 var cookieConfig = require("./cookieConfig");
 require("./db");
 
-var server = module.exports.hapi = new Hapi.Server(8766, options);
+var server = module.exports.hapi = new Hapi.Server(8765, options);
 
 server.pack.require("hapi-auth-cookie", function (err) {
 
@@ -12,7 +12,8 @@ server.pack.require("hapi-auth-cookie", function (err) {
     cookie: cookieConfig.name,
     password: cookieConfig.password,
     ttl: 2592000000,
-    isSecure: false
+    isSecure: false,
+    redirectTo: "/login"
   });
 
   var routes = require("./routes");
