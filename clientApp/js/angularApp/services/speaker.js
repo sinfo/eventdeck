9d997bbf10e5f3e05ca88172031c2f8c16ea2619
Diogo Pais <dpais11@gmail.com>
2014-06-25 11:42:49 +0100

    Revert "All angular"

--- clientApp/js/angularApp/services/speaker.js
+++ clientApp/js/angularApp/services/speaker.js
@@ -3,13 +3,13 @@
 theToolServices
   .factory('SpeakerFactory', function ($resource) {
     return {
-      Speaker: $resource(url_prefix+'/api/speaker/:id', null, {
+      Speaker: $resource('/api/speaker/:id', null, {
         'getAll': {method: 'GET', isArray:true},
         'update': {method: 'PUT'},
         'create': {method: 'POST'},
         'delete': {method: 'DELETE'}
       }),
-      Member: $resource(url_prefix+'/api/member/:id/speakers', null, {
+      Member: $resource('/api/member/:id/speakers', null, {
         'getAll': {method: 'GET', isArray:true}
       })
     };
--- clientApp/js/angularApp/services/tag.js
+++ clientApp/js/angularApp/services/tag.js
@@ -3,13 +3,13 @@
 theToolServices
   .factory('TagFactory', function ($resource) {
     return {
-      Tag: $resource(url_prefix+'/api/tag/:id', null, {
+      Tag: $resource('/api/tag/:id', null, {
         'getAll': {method: 'GET', isArray: true},
         'update': {method: 'PUT'},
         'create': {method: 'POST'},
         'delete': {method: 'DELETE'}
       }),
-      Topic: $resource(url_prefix+'/api/tag/:id/topics', null, {
+      Topic: $resource('/api/tag/:id/topics', null, {
         'getAll': {method: 'GET', isArray: true}
       })
     };
