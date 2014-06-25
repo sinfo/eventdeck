d4b68375145ee6dbe6a134b2579260255a981881
Diogo Pais <dpais11@gmail.com>
2014-06-25 11:42:49 +0100

    Revert "All angular"

--- clientApp/js/angularApp/services/comment.js
+++ clientApp/js/angularApp/services/comment.js
@@ -3,19 +3,19 @@
 theToolServices
   .factory('CommentFactory', function ($resource) {
     return {
-      Comment: $resource(url_prefix+'/api/comment/:id', null, {
+      Comment: $resource('/api/comment/:id', null, {
         'getAll': {method: 'GET', isArray: true},
         'update': {method: 'PUT'},
         'create': {method: 'POST'},
         'delete': {method: 'DELETE'}
       }),
-      Company: $resource(url_prefix+'/api/company/:id/comments', null, {
+      Company: $resource('/api/company/:id/comments', null, {
         'getAll': {method: 'GET', isArray: true}
       }),
-      Speaker: $resource(url_prefix+'/api/speaker/:id/comments', null, {
+      Speaker: $resource('/api/speaker/:id/comments', null, {
         'getAll': {method: 'GET', isArray: true}
       }),
-      Topic: $resource(url_prefix+'/api/topic/:id/comments', null, {
+      Topic: $resource('/api/topic/:id/comments', null, {
         'getAll': {method: 'GET', isArray: true}
       })
     }
