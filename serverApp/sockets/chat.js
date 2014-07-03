5a969b195d25e791dce70fb6bf5deaed1262996e
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-07-03 03:44:24 +0100

    repairing chat #4

--- serverApp/sockets/chat.js
+++ serverApp/sockets/chat.js
@@ -91,6 +91,7 @@ function getChat(chatID, memberID, cb){
   });
 }
 
+
 function getMessages(chatID, dateRef, cb){
   Message.getByChatId({params:{id: chatID, date: dateRef}}, function(response){
     if(response.error) {
@@ -102,6 +103,7 @@ function getMessages(chatID, dateRef, cb){
     }
     cb();
   });
+ /* Message.getByChatLast({params:{id: chatID}}, function(response){});*/
 }
 
 function done(err, room, socket, cb){
