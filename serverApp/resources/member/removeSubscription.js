41c179a64d18cab46be995d1d5c1baf947293d90
Diogo Pais <dpais11@gmail.com>
2014-07-17 23:24:49 +0100

    subscriptions' backend done

--- serverApp/resources/member/removeSubscription.js
+++ serverApp/resources/member/removeSubscription.js
@@ -4,8 +4,18 @@ module.exports = remove;
 
 function remove(request, reply) {
 
-  var member = request.auth.credentials.id;
+  var memberId = request.auth.credentials.id;
 
-  reply("gaita");
+  var source = request.url.path.split("/")[2];
+  var sourceId = request.url.path.split("/")[3];
+
+  Member.update({id: memberId}, {$pull: {"subscriptions.threads": source + "-" + sourceId}}, function (err) {
+    if (err) {
+      reply({error: "There was an error updating the member."});
+    }
+    else {
+      reply({success: "Member updated."});
+    }
+  });
 
 }
