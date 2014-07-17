c39e1d77235772b9faa40cfd52a69ac350e90316
Diogo Pais <dpais11@gmail.com>
2014-07-17 23:24:49 +0100

    subscriptions' backend done

--- serverApp/resources/member/addSubscription.js
+++ serverApp/resources/member/addSubscription.js
@@ -9,7 +9,7 @@ function add(request, reply) {
   var source = request.url.path.split("/")[2];
   var sourceId = request.url.path.split("/")[3];
 
-  Member.update({id: memberId}, {subscriptions: {$push: {threads: source + "-" + sourceId}}}, function (err) {
+  Member.update({id: memberId}, {$push: {"subscriptions.threads": source + "-" + sourceId}}, function (err) {
     if (err) {
       reply({error: "There was an error updating the member."});
     }
