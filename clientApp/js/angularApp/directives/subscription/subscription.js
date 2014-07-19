82dd32a2770338891e06ee11f34290c8576c93aa
Francisco Baio Dias <xicombd@gmail.com>
2014-07-19 17:55:28 +0100

    Subscription directive

---
+++ clientApp/js/angularApp/directives/subscription/subscription.js
@@ -0,0 +1,14 @@
+'use strict';
+
+theToolDirectives
+  .directive('subscription', function () {
+    return {
+      restrict: 'EAC',
+      replace: true,
+      templateUrl: 'views/subscription/button.html',
+      controller: 'SubscriptionController',
+      scope: {
+        thread: '@'
+      }
+    };
+  })
--- serverApp/resources/email/send.js
+++ serverApp/resources/email/send.js
@@ -2,15 +2,42 @@ var Mailgun = require('mailgun').Mailgun;
 var mgConfig = require('./../../../config').mailgun;
 var mg = new Mailgun(mgConfig.api);
 
+var MailComposer = require("mailcomposer").MailComposer;
+var mailcomposer = new MailComposer();
+
 exports = module.exports = send;
 
 function send(message, cb) {
-  mg.sendText('tool@bananamarket.eu',
-    message.to,
-    message.subject,
-    message.text,
-    function(err) { 
-      if(err) { return cb(err) }
-      cb();
+  if(!message.html || message.html == '') {
+    mg.sendText('tool@bananamarket.eu',
+      message.to,
+      message.subject,
+      message.text,
+      function(err) { 
+        if(err) { return cb(err) }
+        cb();
+      });
+  } 
+  else {
+    mailcomposer.setMessageOption({
+      from: 'tool@bananamarket.eu',
+      to: message.to,
+      cc: message.cc,
+      subject: message.subject,
+      replyTo: message.replyTo,
+      body: message.text,
+      html: message.html
     });
+
+    mailcomposer.buildMessage(function(err, rawBody){
+      if(err) { return cb(err) }
+      mg.sendRaw('tool@bananamarket.eu',
+        message.to,
+        rawBody,
+        function(err) { 
+          if(err) { return cb(err) }
+          cb();
+        });
+    });
+  }
 }
