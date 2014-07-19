cb2b34cbfbdf1c6a32ff4f5371d27b622597d9cb
Francisco Baio Dias <xicombd@gmail.com>
2014-07-19 17:55:28 +0100

    Subscription directive

---
+++ clientApp/js/angularApp/controllers/subscription/embed.js
@@ -0,0 +1,55 @@
+"use strict";
+
+theToolController.controller("SubscriptionController", function ($scope, SubscriptionFactory) {
+
+  $scope.loading = true;
+
+  var threadId = $scope.thread.substring($scope.thread.indexOf("-") + 1);
+  var threadKind = $scope.thread.split('-')[0];
+
+  var Factory;
+
+  switch(threadKind) {
+    case 'company':
+      Factory = SubscriptionFactory.Company;
+    break;
+    case 'speaker':
+      Factory = SubscriptionFactory.Speaker;
+    break;
+    case 'topic':
+      Factory = SubscriptionFactory.Topic;
+    break;
+  }
+
+  console.log('THREAD', $scope.thread, threadKind, threadId);
+  console.log('FACTORYYY', SubscriptionFactory.Company, SubscriptionFactory.Speaker, SubscriptionFactory.Topic, Factory);
+
+  $scope.isSubscribed = false;
+
+  $scope.getStatus = function () {
+    Factory.get({id: threadId}, function(response) {
+      console.log('STATUS',response.success)
+      if(response.success == 'subscribed') {
+        $scope.isSubscribed = true;
+      } else {
+        $scope.isSubscribed = false;
+      }
+    })
+  };
+
+  $scope.subscribe = function () {
+    console.log('ADDD', threadKind, threadId);
+    Factory.add({id: threadId}, {}, function(response) {
+      $scope.getStatus();
+    })
+  };
+
+  $scope.unsubscribe = function () {
+    console.log('DELETE', threadKind, threadId);
+    Factory.remove({id: threadId}, function(response) {
+      $scope.getStatus();
+    })
+  };
+
+  $scope.getStatus();
+});
