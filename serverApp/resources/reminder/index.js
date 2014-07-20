4b3c2a2967b11fd5268cb318927ad8b6ab9c79cf
Francisco Baio Dias <xicombd@gmail.com>
2014-07-20 15:48:58 +0100

    DueDates reminder untested

--- serverApp/resources/reminder/index.js
+++ serverApp/resources/reminder/index.js
@@ -1,10 +1,11 @@
-var Notification = require('./../notification');
-var Topic        = require('./../topic');
+var topicDudeDates = require('./topicDueDates');
 
 module.exports = reminder;
 
 function reminder(request, reply) {
 
-  
-
+  topicDudeDates(function(err) {
+    if(err) { return reply({error: 'noooo'}); };
+    reply({ success: 'yeap'});
+  })
 }
