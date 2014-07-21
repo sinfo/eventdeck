7f4cf94c8b145faf9c36d2e5d4e08a5ded765aa8
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-07-22 00:16:09 +0100

    bugs corrections in communication reminders

--- serverApp/resources/communication/getByThreadLast.js
+++ serverApp/resources/communication/getByThreadLast.js
@@ -4,20 +4,7 @@ module.exports = list;
 
 function list(request, reply) {
 
-  var threadId;
-
-  if (request.path.indexOf('/api/company/') != -1) {
-    threadId = 'company-' + request.params.id;
-  }
-  else if (request.path.indexOf('/api/speaker/') != -1) {
-    threadId = 'speaker-' + request.params.id;
-  }
-  else {
-    reply({error: "API path unknown."});
-    return;
-  }
-
-  Communication.findByThreadLast(threadId, function (err, result) {
+  Communication.findByThreadLast(request, function (err, result) {
     if (err) {
       reply({error: "Error getting communications from '" + threadId + "'."});
     }
