f289018d9e88036d1e326e60e837383dcc0884ec
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-07-23 03:04:54 +0100

    communication reminders speaker check

--- serverApp/resources/reminder/communication.js
+++ serverApp/resources/reminder/communication.js
@@ -2,6 +2,7 @@ var async         = require('async');
 var Notification  = require('./../../db/models/notification');
 var notify        = require('./../notification').notify;
 var communication = require('./../communication');
+var Speaker       = require('./../speaker');
 
 module.exports = remind;
 
@@ -15,11 +16,16 @@ function remind(remindDays, done) {
   communication.getThreads(function(threads){
     console.log(threads);
     async.each(threads, function(thread, threadDone) {
+/*      var speaker = thread.split()[1];
+      Speaker.get(speaker, function)*/
       communication.getByThreadLast(thread, function(result){
         if(today.getTime() - result.posted.getTime() > oneDay * remindDays){
           if(result.approved === undefined || result.approved){
             console.log(result);
             notify('toolbot', thread, 'reminder: communications have been innactive for more than ' + remindDays + ' days.', null, result.member);
+          }
+          else{
+            notify('toolbot', thread, 'reminder: communication peding approval for more than ' + remindDays + ' days.', null, result.member);
           }
         }
         threadDone();
