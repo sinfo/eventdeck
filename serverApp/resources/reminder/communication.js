6cd5a5379c658f46d562e1ae7ef64408dc708dd9
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-07-24 02:18:23 +0100

    communication reminders speaker check & approval

--- serverApp/resources/reminder/communication.js
+++ serverApp/resources/reminder/communication.js
@@ -3,6 +3,7 @@ var Notification  = require('./../../db/models/notification');
 var notify        = require('./../notification').notify;
 var communication = require('./../communication');
 var Speaker       = require('./../speaker');
+var Member        = require('./../member');
 
 module.exports = remind;
 
@@ -12,25 +13,48 @@ function remind(remindDays, done) {
   
   var today = new Date();
   var oneDay = (24 * 60 * 60 * 1000);
-  
-  communication.getThreads(function(threads){
-    console.log(threads);
-    async.each(threads, function(thread, threadDone) {
-/*      var speaker = thread.split()[1];
-      Speaker.get(speaker, function)*/
-      communication.getByThreadLast(thread, function(result){
-        if(today.getTime() - result.posted.getTime() > oneDay * remindDays){
-          if(result.approved === undefined || result.approved){
-            console.log(result);
-            notify('toolbot', thread, 'reminder: communications have been innactive for more than ' + remindDays + ' days.', null, result.member);
+  var coordination;
+  var approvalTargets;
+
+  Member.getByRole({params: {id: 'coordination'}}, function(result){
+    if(result.error){
+      console.log(result.error);
+    }
+    else{
+      coordination = result;
+      notifyThreads();
+    }
+  });
+
+  function notifyThreads(){
+    communication.getThreads(function(threads){
+      async.each(threads, function(thread, threadDone) {
+        var speakerId = thread.split('speaker-')[1];
+        Speaker.get({params: {id: speakerId}, auth: {credentials: {id: 'toolbot'}}}, function(speaker){
+          if(speaker.error){
+            console.log(speaker.error);
+            threadDone();
+          }
+          else if(speaker.status !== 'Give Up' && speaker.status !== 'Rejected') {
+            communication.getByThreadLast(thread, function(result){
+              if(today.getTime() - result.posted.getTime() > oneDay * remindDays){
+                if(result.approved === undefined || result.approved){
+                  console.log(thread);
+                  notify('toolbot', thread, 'reminder: communications have been innactive for more than ' + remindDays + ' days.', null, result.member);
+                }
+                else{
+                  approvalTargets = coordination.push(result.member);
+                  notify('toolbot', thread, 'reminder: communication peding approval for more than ' + remindDays + ' days.', null, approvalTargets);
+                }
+              }
+              threadDone();
+            });
           }
           else{
-            notify('toolbot', thread, 'reminder: communication peding approval for more than ' + remindDays + ' days.', null, result.member);
+            console.log('Speaker ' + speaker.id + ' status: ' + speaker.status + ' not reminded.') 
           }
-        }
-        threadDone();
-      });
-    }, done);
-  })
-  
+        });
+      }, done);
+    })
+  }
 }
