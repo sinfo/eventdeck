8b119acdf854a3d6ff2bbc473188965733ab24ec
Francisco Baio Dias <xicombd@gmail.com>
2014-08-02 18:58:07 +0100

    Memeber list redesign

---
+++ clientApp/js/angularApp/directives/member/card.js
@@ -0,0 +1,14 @@
+'use strict';
+
+theToolDirectives
+  .directive('memberCard', function () {
+    return {
+      restrict: 'EAC',
+      replace: true,
+      templateUrl: 'views/member/card.html',
+      controller: 'MemberEmbedController',
+      scope: {
+        member: '=memberObject',
+      }
+    };
+  })
