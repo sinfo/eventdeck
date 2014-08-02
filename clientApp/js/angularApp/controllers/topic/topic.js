eba1c5fd6019b2a666fa488d3e13ef76770eaf3e
Francisco Baio Dias <xicombd@gmail.com>
2014-08-02 21:24:46 +0100

    Several fixes

--- clientApp/js/angularApp/controllers/topic/topic.js
+++ clientApp/js/angularApp/controllers/topic/topic.js
@@ -11,6 +11,12 @@ theToolController.controller('TopicController', function ($rootScope, $scope, $r
     TopicFactory.Topic.get({id: $routeParams.id}, function(result) {
       $scope.topic = result;
 
+      console.log($location.search());
+      if($location.search().editing == true) {
+        $scope.topic.editing=true;
+        console.log('TRUEEE');
+      }
+
       $scope.topic.showComments = true;
 
       NotificationFactory.Topic.getAll({id: $routeParams.id}, function(getData) {
