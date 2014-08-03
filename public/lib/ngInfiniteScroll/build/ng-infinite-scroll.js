fa31b175b5c7f98f26667779e8ba7f5e6e61247e
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-08-03 01:49:11 +0100

    message schema remove by thread

--- public/lib/ngInfiniteScroll/build/ng-infinite-scroll.js
+++ public/lib/ngInfiniteScroll/build/ng-infinite-scroll.js
@@ -139,7 +139,7 @@ mod.directive('infiniteScroll', [
           if (container.scrollTop() <= newHeight - oldHeight + scrollDistance){
             var curPos = container.scrollTop();
             console.log("NewPos: " + (newHeight - oldHeight) + " CurPos: " + curPos);
-            container.scrollTop (newHeight - oldHeight);
+            container.scrollTop(newHeight - oldHeight);
           }
         }
         changeContainer = function(newContainer) {
