ccf5d02a9c9d961cad38bfcf3f8c35f44ca01b6f
Francisco Baio Dias <xicombd@gmail.com>
2014-08-19 22:49:30 +0100

    Close

--- clientApp/js/angularApp/controllers/communication/embed.js
+++ clientApp/js/angularApp/controllers/communication/embed.js
@@ -11,13 +11,8 @@ theToolController.controller("CommunicationEmbedController", function ($rootScop
     $scope.success     = "";
     $scope.error       = "";
 
-    $scope.communication = JSON.parse($scope.communicationJson);
     $scope.communication.editing = false;
     $scope.communication.deleted = false;
-
-    $scope.me = JSON.parse($scope.meJson);
-    $scope.members = JSON.parse($scope.membersJson);
-    $scope.roles = JSON.parse($scope.rolesJson);
 
 
     $scope.saveCommunication = function (communication) {
@@ -30,7 +25,6 @@ theToolController.controller("CommunicationEmbedController", function ($rootScop
 
       CommunicationFactory.Communication.update({id: communication._id}, communication, function (response) {
         communication.editing = false;
-        communication.approved = false;
       });
     }
 
@@ -42,7 +36,6 @@ theToolController.controller("CommunicationEmbedController", function ($rootScop
 
     $scope.setCommunicationStatus = function (communication, status) {
       CommunicationFactory.Communication.update({id: communication._id}, {status: status}, function (response) {
-        $scope.communication.approved = true;
         $scope.communication.status = status;
       });
     };
