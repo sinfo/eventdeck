e40ee6b15a973d1a52b818d82ffcef3f12f10db1
Francisco Baio Dias <xicombd@gmail.com>
2014-09-06 20:09:16 +0100

    Revamped config files, started using bunyan on auth

--- serverApp/db/index.js
+++ serverApp/db/index.js
@@ -1,12 +1,16 @@
 var mongoose = require('mongoose');
-var mongo_url = require('./../../config').mongo.url;
+var mongo_url = require('../../config').mongo.url;
+var log = require('../helpers/logger');
 
 mongoose.connect(mongo_url);
 var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', function(err) {
+  log.error('Connection error:', err);
+});
+
 db.once('open', function (){
-  console.log('Successfuly connected to mongoDB');
+  log.info('Successfuly connected to mongoDB');
 });
 
 module.exports = db;
