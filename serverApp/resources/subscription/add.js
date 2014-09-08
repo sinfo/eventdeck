81fb8e8c580f7b39c24c6c44a5d61b04185a14ec
Francisco Baio Dias <xicombd@gmail.com>
2014-09-08 03:04:24 +0100

    Moar clean up #170

--- serverApp/resources/subscription/add.js
+++ serverApp/resources/subscription/add.js
@@ -1,4 +1,4 @@
-var Member = require("./../../db/models/member.js");
+var Member = require('../../db/models/member');
 
 module.exports = add;
 
@@ -6,15 +6,15 @@ function add(request, reply) {
 
   var memberId = request.auth.credentials.id;
 
-  var source = request.url.path.split("/")[2];
-  var sourceId = request.url.path.split("/")[3];
+  var source = request.url.path.split('/')[2];
+  var sourceId = request.url.path.split('/')[3];
 
-  Member.update({id: memberId}, {$push: {"subscriptions.threads": source + "-" + sourceId}}, function (err) {
+  Member.update({id: memberId}, {$push: {'subscriptions.threads': source + '-' + sourceId}}, function (err) {
     if (err) {
-      return reply({error: "There was an error updating the member."});
+      return reply({error: 'There was an error updating the member.'});
     }
     
-    reply({success: "Member updated."});
+    reply({success: 'Member updated.'});
   });
 
 }
