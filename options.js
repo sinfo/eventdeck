53b81b4d7f33e7abd406cd727884c6faaff4c689
Francisco Baio Dias <xicombd@gmail.com>
2014-09-26 00:35:29 +0100

    Starting to put status on options file #149 & also some #176

---
+++ options.js
@@ -0,0 +1,39 @@
+var options = {};
+
+options.statuses = {
+  speaker: [
+    { name: 'Selected', color: '#5319e7'},
+    { name: 'Approved', color: '#0052cc'},
+    { name: 'Contacted', color: '#fbca04'},
+    { name: 'In Conversations', color: '#207de5'},
+    { name: 'Accepted', color: '#009800'},
+    { name: 'Rejected', color: '#e11d21'},
+    { name: 'Give Up', color: '#333'}
+  ],
+  company: [
+    { name: 'Contacted', color: '#fbca04'},
+    { name: 'In Conversations', color: '#207de5'},
+    { name: 'In Negotiations', color: '#006b75'},
+    { name: 'Closed Deal', color: '#009800'},
+    { name: 'Rejected', color: '#e11d21'},
+    { name: 'Give Up', color: '#333'} 
+  ]
+};
+
+options.roles = [
+  { name: 'Public Relations', id: 'public-relations' },
+  { name: 'Marketing', id: 'marketing' },
+  { name: 'Internal Relations', id: 'internal-relations' },
+  { name: 'Events', id: 'events' },
+  { name: 'Innovation Awards', id: 'innovation-awards' },
+  { name: 'Design', id: 'design' },
+  { name: 'Development Team', id: 'development-team' },
+  { name: 'Coordination', id: 'coordination' },
+  { name: 'Treasury', id: 'treasury' },
+  { name: 'External Relations', id: 'external-relations' },
+  { name: 'Strategic Partnerships', id: 'strategic-partnerships' },
+  { name: 'Sys Admin', id: 'sys-admin' },
+  { name: 'Marketing Manager', id: 'marketing-manager' }
+];
+
+module.exports = options;
