90294ed2a3d599c7c2200658a19cdf4c90c99f99
Francisco Baio Dias <xicombd@gmail.com>
2014-10-05 22:50:33 +0100

    Revamping tag

--- server/resources/tag/index.js
+++
@@ -1,5 +0,0 @@
-exports.create = require('./create');
-exports.delete = require('./delete');
-exports.get    = require('./get');
-exports.list   = require('./list');
-exports.update = require('./update');
