d2ab4d68d331517c90299d2d013c6832ae5022d9
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-11-04 01:36:40 +0000

    cleaning db models

--- server/db/models/subscription.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var schema = new mongoose.Schema({
-  member: String,
-  thread: String
-});
-
-var Subscription = module.exports = mongoose.model('Subscription', schema);
