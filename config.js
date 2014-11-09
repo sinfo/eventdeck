354e659e2625d54ead4dd26a34e6b5f71d361a2b
Duarte Paiva <duarte.paiva@sinfo.org>
2014-11-09 17:57:51 +0000

    oopsie daisy fixed config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ var config = {
 };
 
 config.mongo = {
-  url: process.env.EVENTDECK_MONGO_URL || 'mongodb://localhost/sinfo'
+  url: process.env.EVENTDECK_MONGO_URL || 'mongodb://localhost/deck'
 };
 
 config.cookie = {
