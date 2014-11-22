0200a040f7e74673ec926ff1773242e690c6de93
Francisco Baio Dias <xicombd@gmail.com>
2014-11-22 19:42:23 +0000

    Fixing bugs on company by member table #220

--- client/js/views/memberCompanies.js
+++ client/js/views/memberCompanies.js
@@ -1,8 +1,7 @@
 var View = require('ampersand-view');
 var templates = require('client/js/templates');
 var Company = require('client/js/models/company');
-var MemberCompaniesRow = require('client/js/views/memberCompaniesRow');
-var AmpersandCollection = require('ampersand-collection'); 
+var AmpersandCollection = require('ampersand-collection');
 
 module.exports = View.extend({
   template: templates.cards.memberCompanies,
@@ -14,28 +13,26 @@ module.exports = View.extend({
       name: 'src'
     },
     'model.viewUrl': {
-        type: 'attribute',
-        hook: 'name',
-        name: 'href'
+      type: 'attribute',
+      hook: 'name',
+      name: 'href'
     }
   },
   initialize: function() {
     var self = this;
     this.collection = null;
-    var companies = app.companies.filter(function(company){
-      console.log("FILTER", company.participation.member, self.model.id);
-      return company.participation && company.participation.member == self.model.id;
+    app.companies.on('sync', function() {
+      var companies = app.companies.filter(function(company){
+        return company.participation && company.participation.member == self.model.id;
+      });
+
+      self.collection = new AmpersandCollection(companies, {model: Company});
+      self.render();
     });
-    this.collection = new AmpersandCollection(companies, {model: Company});
-    console.log("COMPANIES", companies);
-    console.log("MODEL", self.model);
-    console.log("MODEL ID", self.model.id);
-    console.log("INITIALIZE", this.collection.serialize());
-    this.render();
+
   },
   render: function () {
     this.renderWithTemplate();
-    console.log(this.collection.serialize());
     this.renderCollection(this.collection, MemberCompaniesRow, this.queryByHook('companiesContainer'));
     if (!this.collection.length) {
       //this.fetchCollection();
@@ -43,4 +40,20 @@ module.exports = View.extend({
   },
 });
 
+var MemberCompaniesRow = View.extend({
+  template: templates.cards.memberCompaniesRow,
+  bindings: {
+    'model.name': '[data-hook~=company-name]',
+    'model.img': {
+      type: 'attribute',
+      hook: 'company-img',
+      name: 'src'
+    },
+    'model.viewUrl': {
+      type: 'attribute',
+      hook: 'company-url',
+      name: 'href'
+    }
+  },
+});
 
