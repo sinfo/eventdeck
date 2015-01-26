88ed36d61a66fe205715de3e16383518cb897fdf
Francisco Baio Dias <xicombd@gmail.com>
2015-01-26 22:29:47 +0000

    Fixing tags (added missing files) #224

---
+++ client/js/views/topicTags.js
@@ -0,0 +1,49 @@
+/*global app*/
+var log = require('bows')('topic-tags');
+var View = require('ampersand-view');
+var templates = require('client/js/templates');
+var SubCollection = require('ampersand-subcollection');
+
+var TagView = View.extend({
+  template: templates.partials.topics.tag,
+  bindings: {
+    'model.name': '[data-hook~=tag]',
+    'model.style': {
+      type: 'attribute',
+      hook: 'tag',
+      name: 'style'
+    },
+  }
+});
+
+
+module.exports = View.extend({
+  template: templates.partials.topics.tags,
+  initialize: function () {
+    var self = this;
+    if(!app.tags.length) {
+      return self.filterTags();
+    }
+
+    app.tags.fetch({success: function() {
+      self.filterTags();
+    }});
+  },
+  filterTags: function () {
+    var self = this;
+    if(!this.model.tagsDetails.length) {
+      this.model.tagsDetails = new SubCollection(app.tags, {
+        filter: function (tag) {
+          return self.model.tags.indexOf(tag.id) != -1;
+        }
+      });
+    }
+    this.render();
+  },
+  render: function () {
+    var self = this;
+    this.renderWithTemplate();
+    this.renderCollection(self.model.tagsDetails, TagView, this.queryByHook('topic-tags'));
+    // log('Rendering', self.model.tagsDetails.length, 'tag(s)');
+  },
+});
