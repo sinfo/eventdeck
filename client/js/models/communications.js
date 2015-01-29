e4ae63ab613f2d746111678ebabd15cb8ec29161
Diogo Pais <dpais11@gmail.com>
2015-01-29 19:17:28 +0000

    new communications form on top and inverted communications order. closed #260

--- client/js/models/communications.js
+++ client/js/models/communications.js
@@ -4,7 +4,10 @@ var communication = require('./communication');
 module.exports = function(url) {
   return AmpCollection.extend({
     model: communication,
-    url: url
+    url: url,
+    comparator: function (o1, o2) {
+      return o1.posted < o2.posted;
+    }
   });
 };
 
