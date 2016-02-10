266176faa25dec885a45f9eecdec3cae57587197
Duarte Paiva <dmpaiva@github.com>
2016-02-10 01:01:28 +0000

    silly me forgot to add a new model

---
+++ client/js/models/sessionsEvent.js
@@ -0,0 +1,11 @@
+var AmpCollection = require('ampersand-rest-collection');
+var session = require('./session');
+
+
+module.exports = AmpCollection.extend({
+  model: session,
+  url: '/api/sessions',
+  initialize: function() {
+  	this.url = '/api/sessions?event=' + app.me.selectedEvent;
+  }
+});
