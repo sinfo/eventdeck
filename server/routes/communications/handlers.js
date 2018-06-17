8b2fd6777538956c810d6c2c29a087c4eb49a885
fchamicapereira <fchamicapereira@gmail.com>
2018-06-17 15:24:16 +0100

    Added SINFO26's speakers template

--- server/routes/communications/handlers.js
+++ server/routes/communications/handlers.js
@@ -97,7 +97,7 @@ exports.getView = {
   ],
   handler: function (request, reply) {
     // console.log(request.auth);
-    reply.view('speakers25', {speaker: request.pre.speaker.name, paragraph: request.pre.communication.text, member: request.pre.member.name})
+    reply.view('speakers26', {speaker: request.pre.speaker.name, paragraph: request.pre.communication.text, member: request.pre.member.name})
   },
   description: 'Gets a communication with an initial template view'
 }
