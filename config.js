0f43cb2652259acd9fdc1aabe0c8a3de3c5c4f8c
fchamicapereira <fchamicapereira@gmail.com>
2018-11-13 18:02:40 +0000

    fixed cors

--- config.js
+++ config.js
@@ -76,7 +76,7 @@ config.images = {
 
 config.cors = {
   origin: process.env.NODE_ENV === 'production'
-    ? ['https://sinfo.org', 'https://deck.sinfo.org', 'https://cannon.sinfo.org', 'https://corlief.sinfo.org']
+    ? ['https://www.sinfo.org', 'https://sinfo.org', 'https://deck.sinfo.org', 'https://cannon.sinfo.org', 'https://corlief.sinfo.org']
     : ['http://localhost:4200', 'https://sinfo.org', 'https://deck.sinfo.org', 'https://cannon.sinfo.org', 'https://corlief.sinfo.org']
 ,
   additionalHeaders: ['Only-Public'],
